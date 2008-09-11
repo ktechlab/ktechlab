b7c9d76e99dc6e5ee7d271df6a1f61d3ef01335c
Zoltan Padrah <zoltan_padrah@users.sf.net>
2008-09-11 16:16:31 +0000

    Added {Junction|Pin}Node, FlowICNDDocument, {Input|Junction|Output}FlowNode to repository.
    git-svn-id: https://ktechlab.svn.sourceforge.net/svnroot/ktechlab/trunk@119 76876f85-f02d-0410-881d-fd707285c4ba

---
+++ src/electronics/junctionnode.cpp
@@ -0,0 +1,49 @@
+//
+// C++ Implementation: junctionnode
+//
+// Description: 
+//
+//
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "junctionnode.h"
+
+#include "pin.h"
+#include "component.h"
+
+#include <qpainter.h>
+
+
+JunctionNode::JunctionNode(ICNDocument* icnDocument, int dir, const QPoint& pos, QString* id): 
+		ECNode(icnDocument, Node::ec_junction, dir, pos, id)
+{
+}
+
+
+JunctionNode::~JunctionNode()
+{
+}
+
+
+void JunctionNode::drawShape( QPainter & p )
+{
+	initPainter( p );
+
+	double v = pin() ? pin()->voltage() : 0.0;
+	QColor voltageColor = Component::voltageColor( v );
+
+	QPen pen = p.pen();
+
+	if ( isSelected() )
+		pen = m_selectedColor;
+	else if ( m_bShowVoltageColor )
+		pen = voltageColor;
+
+	
+	p.setPen( pen );
+	p.setBrush( pen.color() );
+	p.drawRect( -1, -1, 3, 3 );
+	deinitPainter( p );
+}
