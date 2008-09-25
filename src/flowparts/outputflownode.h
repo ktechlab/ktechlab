a1eaeec24b311014f72ecf244d40bf0ff5a8075e
zoltan_padrah <zoltan_padrah@76876f85-f02d-0410-881d-fd707285c4ba>
2008-09-25 16:35:14 +0000

    Cleared FPNode class from code that doesn't belong there.
    {Input,Junction,Ouptut}FlowNodes now have some members :P
    
    git-svn-id: https://ktechlab.svn.sourceforge.net/svnroot/ktechlab/trunk@145 76876f85-f02d-0410-881d-fd707285c4ba

--- src/flowparts/outputflownode.h
+++ src/flowparts/outputflownode.h
@@ -1,7 +1,7 @@
 //
 // C++ Interface: outputflownode
 //
-// Description: 
+// Description:
 //
 //
 // Copyright: See COPYING file that comes with this distribution
@@ -17,12 +17,34 @@
 */
 class OutputFlowNode : public FPNode
 {
-public:
-    OutputFlowNode(ICNDocument* _icnView, int dir, const QPoint& pos, QString* id = 0L);
+	public:
+		OutputFlowNode ( ICNDocument* _icnView, int dir, const QPoint& pos, QString* id = 0L );
 
-    ~OutputFlowNode();
+		~OutputFlowNode();
 
+		/**
+		 * Returns true if the node can accept input connections. This will depend
+		 * on the node type and number of input / output connections.
+		 */
+		virtual bool acceptInput() const;
+		/**
+		 * Returns true if the node can accept output connections. This will depend
+		 * on the node type and number of input / output connections.
+		 */
+		virtual bool acceptOutput() const;
+		
+		/**
+		 * Registers an input connector (i.e. this is the end node) as connected
+		 * to this node.
+		 */
+		virtual void addInputConnector( Connector * const connector );		
+		
+	public slots:
 
+		/**
+		 * Draw shape. Note that this has to remain public.
+		 */
+		virtual void drawShape ( QPainter & p );
 };
 
 #endif
