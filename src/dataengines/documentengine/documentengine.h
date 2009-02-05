17c3fe2a08dc7561262d2348605a13661e5f3a92
Julian Bäume <julian@svg4all.de>
2009-02-05 16:18:56 +0100

    add DocumentEngine
    no code in here for now, need to fix UI first

---
+++ src/dataengines/documentengine/documentengine.h
@@ -0,0 +1,26 @@
+/******************************************************************************
+ * DocumentEngine Class - Model representing documents                        *
+ *                                                                            *
+ * Copyright (C) 2008 Julian Bäume <julian@svg4all.de>                        *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or              *
+ * modify it under the terms of the GNU General Public License as             *
+ * published by the Free Software Foundation; either version 2 of             *
+ * the License, or (at your option) any later version.                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.      *
+ ******************************************************************************/
+
+#include <Plasma/DataEngine>
+
+/**
+*/
+class DocumentEngine : public Plasma::DataEngine
+{
+};
