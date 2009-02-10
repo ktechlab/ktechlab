f87869729f66895497427e6a374edb6b33bf1cbe
Alan Grimes <AlonzoTG@users.sf.net>
2009-02-10 19:49:03 +0000

    some of intended matrix changes didn't go through.
    git-svn-id: https://ktechlab.svn.sourceforge.net/svnroot/ktechlab/trunk@196 76876f85-f02d-0410-881d-fd707285c4ba

--- src/electronics/simulation/matrix.cpp
+++ src/electronics/simulation/matrix.cpp
@@ -21,27 +21,27 @@
 /// Minimum value before an entry is deemed "zero"
 const double epsilon = 1e-50;
 
-Matrix::Matrix( CUI n, CUI m )
+Matrix::Matrix(CUI n, CUI m)
 	: m_n(n)
 {
 	unsigned int size = m_n + m;
 
 	m_mat = new QuickMatrix(size);
-	m_lu = new QuickMatrix(size);
+	m_lu  = new QuickMatrix(size);
 
-	m_y = new double[size];
+	m_y     = new double[size];
 	m_inMap = new int[size];
 
-	for ( unsigned int i=0; i<size; i++ )
+	for(unsigned int i = 0; i < size; i++ )
 		m_inMap[i] = i;
 }
 
 Matrix::~Matrix()
 {
-	delete m_mat;
-	delete m_lu;
-	delete [] m_y;
-	delete [] m_inMap;
+	delete   m_mat;
+	delete   m_lu;
+	delete[] m_y;
+	delete[] m_inMap;
 }
 
 void Matrix::zero()
@@ -52,17 +52,17 @@ void Matrix::zero()
 	m_lu->fillWithZero();
 	unsigned int size = m_mat->size_m();
 
-	for ( unsigned int i = 0; i < size; i++ )
+	for(unsigned int i = 0; i < size; i++ )
 		m_inMap[i] = i;
 
 	max_k = 0;
 }
 
-void Matrix::swapRows( CUI a, CUI b )
+void Matrix::swapRows(CUI a, CUI b)
 {
-	if ( a == b ) return;
-	m_mat->swapRows( a, b );
-	
+	if(a == b) return;
+	m_mat->swapRows(a, b);
+
 	const int old = m_inMap[a];
 	m_inMap[a] = m_inMap[b];
 	m_inMap[b] = old;
@@ -73,35 +73,35 @@ void Matrix::swapRows( CUI a, CUI b )
 void Matrix::performLU()
 {
 	unsigned int n = m_mat->size_m();
-	if ( n == 0 ) return;
+	if(n == 0) return;
 
 	// Copy the affected segment to LU
-	for ( uint i=max_k; i<n; i++ ) {
-		for ( uint j=max_k; j<n; j++ ) {
+	for(uint i = max_k; i < n; i++) {
+		for(uint j = max_k; j < n; j++) {
 			(*m_lu)[i][j] = (*m_mat)[i][j];
 		}
 	}
 
 	// LU decompose the matrix, and store result back in matrix
-	for ( uint k=0; k<n-1; k++ ) {
+	for(uint k = 0; k < n-1; k++ ) {
 
 		double *const lu_K_K = &(*m_lu)[k][k];
-		unsigned foo = std::max(k,max_k)+1;
+		unsigned foo = std::max(k, max_k) + 1;
 
 // detect singular matrixes...
-		if ( std::abs(*lu_K_K) < 1e-10 ) {
-			if(*lu_K_K < 0. ) *lu_K_K = -1e-10;
+		if(std::abs(*lu_K_K) < 1e-10) {
+			if(*lu_K_K < 0.) *lu_K_K = -1e-10;
 			else *lu_K_K = 1e-10;
 		}
 // #############
 
-		for ( uint i= foo; i<n; i++ ) {
+		for(uint i = foo; i < n; i++) {
 			(*m_lu)[i][k] /= *lu_K_K;
 		}
 
-		for ( uint i=std::max(k,max_k)+1; i<n; i++ ) {
+		for(uint i = std::max(k, max_k) + 1; i < n; i++) {
 			const double lu_I_K = (*m_lu)[i][k];
-			if ( std::abs(lu_I_K) > 1e-12 )	{
+			if(std::abs(lu_I_K) > 1e-12) {
 				m_lu->partialSAF(k, i, foo, -lu_I_K);
 			}
 		}
@@ -110,49 +110,47 @@ void Matrix::performLU()
 	max_k = n;
 }
 
-void Matrix::fbSub( QuickVector *b )
+void Matrix::fbSub(QuickVector *b)
 {
 	unsigned int size = m_mat->size_m();
 
-	for ( uint i=0; i<size; i++ ) {
+	for(uint i = 0; i < size; i++) {
 		m_y[m_inMap[i]] = (*b)[i];
 	}
 
 	// Forward substitution
-	for ( uint i = 1; i<size; i++ ) {
+	for(uint i = 1; i < size; i++) {
 		double sum = 0;
-		for ( unsigned int j=0; j<i; j++ )
-		{
-			sum += (*m_lu)[i][j]*m_y[j];
+		for(unsigned int j = 0; j < i; j++ ) {
+			sum += (*m_lu)[i][j] * m_y[j];
 		}
 		m_y[i] -= sum;
 	}
 
 	// Back substitution
 	m_y[size - 1] /= (*m_lu)[size - 1][size - 1];
-	for ( int i = size - 2; i >= 0; i-- )
-	{
+	for(int i = size - 2; i >= 0; i-- ) {
 		double sum = 0;
-		for ( uint j=i+1; j<size; j++ )
-		{
-			sum += (*m_lu)[i][j]*m_y[j];
+		for(uint j = i + 1; j < size; j++) {
+			sum += (*m_lu)[i][j] * m_y[j];
 		}
+
 		m_y[i] -= sum;
 		m_y[i] /= (*m_lu)[i][i];
 	}
 
 // I think we don't need to reverse the mapping because we only permute rows, not columns. 
-	for ( uint i=0; i<size; i++ )
+	for(uint i = 0; i < size; i++ )
 		(*b)[i] = m_y[i];
 }
 
-void Matrix::multiply(const QuickVector *rhs, QuickVector *result )
+void Matrix::multiply(const QuickVector *rhs, QuickVector *result)
 {
-	if ( !rhs || !result ) return;
+	if(!rhs || !result) return;
 	result->fillWithZeros();
 
 	unsigned int size = m_mat->size_m();
-	for ( uint _i=0; _i<size; _i++ ) {
+	for(uint _i = 0; _i < size; _i++) {
 		uint i = m_inMap[_i];
 /* hmm, we should move the resolution of pointers involving i out of the inner loop but
 there doesn't appear to be a way to obtain direct pointers into our classes inner structures.
@@ -161,7 +159,7 @@ instance... Furthermore, our matrix class has an accelerator for this operation 
 ignorant of row permutations and it allocates new memory for the result matrix, breaking the
 interface of this method. 
 */
-		for ( uint j=0; j<size; j++ ) {
+		for(uint j = 0; j < size; j++) {
 			result->atAdd(_i, (*m_mat)[i][j] * (*rhs)[j]);
 		}
 	}
@@ -170,12 +168,11 @@ interface of this method.
 void Matrix::displayMatrix()
 {
 	uint n = m_mat->size_m();
-	for ( uint _i=0; _i<n; _i++ )
-	{
+	for(uint _i = 0; _i < n; _i++) {
+
 		uint i = m_inMap[_i];
-		for ( uint j=0; j<n; j++ )
-		{
-			if ( j > 0 && (*m_mat)[i][j] >= 0 ) kdDebug() << "+";
+		for(uint j = 0; j < n; j++) {
+			if(j > 0 && (*m_mat)[i][j] >= 0 ) kdDebug() << "+";
 			kdDebug() << (*m_mat)[i][j] << "("<<j<<")";
 		}
 		kdDebug()  << endl;
@@ -185,29 +182,26 @@ void Matrix::displayMatrix()
 void Matrix::displayLU()
 {
 	uint n = m_mat->size_m();
-	for ( uint _i=0; _i<n; _i++ ) {
+	for(uint _i = 0; _i < n; _i++) {
 		uint i = m_inMap[_i];
-// 		uint i = _i;
-		for ( uint j=0; j<n; j++ )
-		{
+
+		for(uint j = 0; j < n; j++ ) {
 			if ( j > 0 && (*m_lu)[i][j] >= 0 ) std::cout << "+";
-			std::cout << (*m_lu)[i][j] << "("<<j<<")";
+			std::cout << (*m_lu)[i][j] << "(" << j << ")";
 		}
 		std::cout << std::endl;
 	}
 
 	std::cout << "m_inMap:    ";
-	for ( uint i=0; i<n; i++ ) {
-		std::cout << i<<"->"<<m_inMap[i]<<"  ";
+	for(uint i = 0; i < n; i++ ) {
+		std::cout << i << "->" << m_inMap[i] << "  ";
 	}
 
 	std::cout << std::endl;
 	/*cout << "m_outMap:   ";
-	for ( uint i=0; i<n; i++ )
-	{
-		cout << i<<"->"<<m_outMap[i]<<"  ";
+	for(uint i = 0; i < n; i++) {
+		cout << i << "->" << m_outMap[i] << "  ";
 	}
 	cout << endl;*/
 }
-
 
