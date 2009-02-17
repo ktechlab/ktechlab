795a1b1baf8b7139e91faf32f7d465946203535a
AlonzoTG <AlonzoTG@76876f85-f02d-0410-881d-fd707285c4ba>
2009-02-17 17:32:00 +0000

    tweaked the 555 class.
    git-svn-id: https://ktechlab.svn.sourceforge.net/svnroot/ktechlab/trunk@202 76876f85-f02d-0410-881d-fd707285c4ba

--- src/electronics/components/ec555.cpp
+++ src/electronics/components/ec555.cpp
@@ -19,9 +19,9 @@
 #include <klocale.h>
 #include <qpainter.h>
 
-Item* EC555::construct( ItemDocument *itemDocument, bool newItem, const char *id )
+Item* EC555::construct(ItemDocument *itemDocument, bool newItem, const char *id)
 {
-	return new EC555( (ICNDocument*)itemDocument, newItem, id );
+	return new EC555((ICNDocument*)itemDocument, newItem, id);
 }
 
 LibraryItem* EC555::libraryItem()
@@ -35,65 +35,61 @@ LibraryItem* EC555::libraryItem()
 		EC555::construct );
 }
 
-EC555::EC555( ICNDocument *icnDocument, bool newItem, const char *id )
-	: Component( icnDocument, newItem, (id) ? id : "555" )
+EC555::EC555(ICNDocument *icnDocument, bool newItem, const char *id)
+	: Component(icnDocument, newItem, (id) ? id : "555")
 {
 	m_name = i18n("555");
 // 	m_pins = QStringList::split( ',', "Gnd,Trg,Out,Res,CV,Th,Dis,Vcc" );
 // 	m_pins = QStringList::split( ',', "Dis,Th,Trg,Gnd,CV,Out,Res,Vcc" );
-	
-	old_com1 = false;
-	old_com2 = false;
-	old_q = false;
-	
-	setSize( -32, -32, 64, 64 );
+
+	m_com1 = false;
+	m_com2 = false;
+	m_q    = false;
+
+	setSize(-32, -32, 64, 64);
 
 	// Pins down left
-	
 	// Pin 7
-	discharge = createPin( -40, -16, 0, "Dis" )->pin();
-	addDisplayText( "dis", QRect( -32, -24, 24, 16 ), "Dis" );
+	discharge = createPin(-40, -16, 0, "Dis")->pin();
+	addDisplayText("dis", QRect(-32, -24, 24, 16), "Dis");
 	
 	// Pin 6
 	threshold = createPin( -40, 0, 0, "Th" )->pin();
-	addDisplayText( "th", QRect( -32, -8, 24, 16 ), "Th" );
+	addDisplayText("th", QRect(-32, -8, 24, 16), "Th");
 	
 	// Pin 2
-	trigger = createPin( -40, 16, 0, "Trg" )->pin();
-	addDisplayText( "trg", QRect( -32, 8, 24, 16 ), "Trg" );
+	trigger = createPin(-40, 16, 0, "Trg")->pin();
+	addDisplayText("trg", QRect(-32, 8, 24, 16), "Trg");
 
 	// Top two
-	
 	// Pin 8
-	vcc = createPin( -16, -40, 90, "Vcc" )->pin();
-	addDisplayText( "vcc", QRect( -24, -32, 16, 8 ), "+" );
-	
+	vcc = createPin(-16, -40, 90, "Vcc")->pin();
+	addDisplayText("vcc", QRect(-24, -32, 16, 8), "+");
+
 	// Pin 4
-	reset = createPin( 16, -40, 90, "Res" )->pin();
-	addDisplayText( "res", QRect( 8, -28, 16, 16 ), "Res" );
-	
+	reset = createPin(16, -40, 90, "Res")->pin();
+	addDisplayText("res", QRect(8, -28, 16, 16), "Res");
+
 	// Bottom two
-	
 	// Pin 1
-	ground = createPin( -16, 40, 270, "Gnd" )->pin();
-	addDisplayText( "gnd", QRect( -24, 20, 16, 8 ), "-" );
-	
+	ground = createPin(-16, 40, 270, "Gnd")->pin();
+	addDisplayText("gnd", QRect(-24, 20, 16, 8), "-");
+
 	// Pin 5
-	control = createPin( 16, 40, 270, "CV" )->pin();
-	addDisplayText( "cv", QRect( 8, 12, 16, 16 ), "CV" );
-	
+	control = createPin(16, 40, 270, "CV")->pin();
+	addDisplayText("cv", QRect(8, 12, 16, 16), "CV");
+
 	// Output on right
-	
 	// Pin 3
-	output = createPin( 40, 0, 180, "Out" )->pin();
-	addDisplayText( "out", QRect( 8, -8, 16, 16 ), "Out" );
+	output = createPin(40, 0, 180, "Out")->pin();
+	addDisplayText("out", QRect(8, -8, 16, 16), "Out");
 	
-	m_r1 = createResistance( vcc, control, 5e3 );
-	m_r23 = createResistance( control, ground, 1e4 );
-	m_po_sink = createResistance( output, ground, 0. );
-	m_po_source = createResistance( output, vcc, 0. );
+	m_r1          = createResistance(vcc, control, 5e3);
+	m_r23         = createResistance(control, ground, 1e4);
+	m_po_sink     = createResistance(output, ground, 10);
+	m_po_source   = createResistance(output, vcc, 0);
 	m_po_source->setConductance(0.);
-	m_r_discharge = createResistance( discharge, ground, 0. );
+	m_r_discharge = createResistance(discharge, ground, 0.);
 }
 
 EC555::~EC555()
@@ -112,33 +108,27 @@ void EC555::stepNonLogic()
 	double v_trigger = trigger->voltage();
 	double v_reset = reset->voltage();
 	double v_vcc = vcc->voltage();
-	double v_r = (v_control+v_ground)/2;
-	
-	bool com1 = (v_threshold == v_control ) ? old_com1 : ( v_threshold < v_control );
-	bool com2 = (v_r == v_trigger )         ? old_com2 : ( v_r > v_trigger );
-	bool reset = v_reset >= (v_control - v_ground) / 2 + v_ground;
+	double v_reset_t = (v_control + v_ground) / 2;
 
-	old_com1 = com1;
-	old_com2 = com2;
-	
-	bool r = !(reset && com1);
-	bool s = com2;
-	bool q = old_q;
+	if(v_threshold != v_control) { m_com1 = v_threshold < v_control; }
+	if(v_reset_t   != v_trigger) { m_com2 = v_reset_t   > v_trigger; }
+
+	bool reset_asserted = v_reset >= v_reset_t;
+	bool r     = (reset_asserted && m_com1);
 
 	if ( v_vcc - v_ground >= 2.5 ) {
-		if ( s && !r ) {
-			q = true;
-		} else if ( r && !s ) {
-			q = false;
+		if(m_com2 && r) {
+			m_q = true;
+		} else if(!(r || m_com2)) {
+			m_q = false;
 		}
 	} else {
-		q = false;
+		m_q = false;
 	}
 
-	old_q = q;
 	m_r_discharge->setConductance(0.);
 	
-	if (q) {
+	if(m_q) {
 		m_po_source->setResistance(10.);
 		m_po_sink->setConductance(0.);
 	} else {
