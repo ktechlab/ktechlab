b7c237235009354ef87aca795d4fa962921cd3a8
Julian Bäume <julian@svg4all.de>
2009-02-21 16:28:11 +0100

    first work on the DocumentEngine
    remove old stuff I used for testing, it was never intended to be
    commited.. but well ;)

--- src/dataengines/documentengine/documentengine.cpp
+++ src/dataengines/documentengine/documentengine.cpp
@@ -1,14 +1,20 @@
 #include "documentengine.h"
 #include "shell/core.h"
-#include "shell/uicontroller.h"
-#include "shell/mainwindow.h"
+#include "shell/documentcontroller.h"
 
 #include <KDebug>
+#include <Plasma/DataEngineManager>
 
 
 DocumentEngine::DocumentEngine( QObject* parent, const QVariantList& args )
-    : Plasma::DataEngine( parent, args )
+    :   Plasma::DataEngine( parent, args ),
+        m_core( KTechLab::Core::self() ),
+        m_disabled( false )
 {
+    if ( !m_core ) {
+        kError() << "Please only do use this DataEngine from a started KTechLab session." << endl;
+        m_disabled = true;
+    }
 }
 
 DocumentEngine::~DocumentEngine()
@@ -31,13 +37,33 @@ bool DocumentEngine::sourceRequestEvent( const QString &name )
 
 bool DocumentEngine::updateSourceEvent( const QString &source )
 {
-    KTechLab::Core *core = KTechLab::Core::self();
-    QString foo = core->uiControllerInternal()->defaultMainWindow()->objectName();
-    kDebug() << source << endl;
-    kDebug() << foo << endl;
+    if ( m_disabled ) {
+        return false;
+    }
+
+    const KDevelop::IDocumentController *docController = m_core->documentController();
+    QList<KDevelop::IDocument*> docList = docController->openDocuments();
+    QStringList urlList;
+    KDevelop::IDocument *document = 0;
+    foreach (KDevelop::IDocument *doc, docList) {
+        urlList << doc->url().prettyUrl();
+        // check if this document is requested as source, store pointer
+        if ( source == doc->url().prettyUrl() ) {
+            document = doc;
+        }
+    }
+
     if ( source == I18N_NOOP("opened") ) {
-        setData( source, I18N_NOOP("test"), QVariant(i18n("Blair") ));
-        setData( source, I18N_NOOP("test2"), QVariant(foo) );
+        setData( source, I18N_NOOP("documentCount"), QString::number( docList.count() ) );
+        setData( source, I18N_NOOP("documentList"), urlList );
+
+        return true;
+    }
+    // specific document is chosen
+    if ( document ) {
+        setData( source, I18N_NOOP("available"), true );
+        setData( source, I18N_NOOP("mime"), document->mimeType()->name() );
+        //setData( source, );
         return true;
     }
 
