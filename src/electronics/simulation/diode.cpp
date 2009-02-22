76e638e4954091ed38b70f1132b16ab245c0dae8
Alan Grimes <AlonzoTG@users.sf.net>
2009-02-22 02:38:48 +0000

    first attempt at fixing nonlinear.
    git-svn-id: https://ktechlab.svn.sourceforge.net/svnroot/ktechlab/trunk@211 76876f85-f02d-0410-881d-fd707285c4ba

--- src/electronics/simulation/diode.cpp
+++ src/electronics/simulation/diode.cpp
@@ -22,7 +22,6 @@ DiodeSettings::DiodeSettings()
 	reset();
 }
 
-
 void DiodeSettings::reset()
 {
 	I_S = 1e-15;
@@ -33,16 +32,16 @@ void DiodeSettings::reset()
 //END class Diode Settings
 
 
-
 //BEGIN class Diode
 Diode::Diode()
 	: NonLinear()
 {
 	m_numCNodes = 2;
 	g_new = g_old = I_new = I_old = V_prev = 0.0;
+//	g_new = g_old = I_new = V_prev = 0.0;
+
 	updateLim();
 }
-
 
 Diode::~Diode()
 {
@@ -51,58 +50,52 @@ Diode::~Diode()
 void Diode::add_initial_dc()
 {
 	g_new = g_old = I_new = I_old = V_prev = 0.0;
+//	g_new = g_old = I_new = V_prev = 0.0;
+
 	update_dc();
 }
 
-
 double Diode::current() const
 {
-	if (!b_status)
-		return 0.0;
-	
-	double I;
-	calcIg( p_cnode[0]->v - p_cnode[1]->v, & I, 0 );
-	
+	if (!b_status) return 0.0;
+
+	double I, g;
+	calcIg(p_cnode[0]->v - p_cnode[1]->v, &I, &g);
 	return I;
 }
 
-
 void Diode::updateCurrents()
 {
-	if (!b_status)
-		return;
+	if (!b_status) return;
 	
 	m_cnodeI[1] = current();
 	m_cnodeI[0] = -m_cnodeI[1];
 }
 
-
 void Diode::update_dc()
 {
-	if (!b_status)
-		return;
-	
+	if (!b_status) return;
+
 	calc_eq();
-	
-	A_g( 0, 0 ) += g_new - g_old;
-	A_g( 1, 1 ) += g_new - g_old;
-	A_g( 0, 1 ) -= g_new - g_old;
-	A_g( 1, 0 ) -= g_new - g_old;
-	
-	b_i( 0 ) -= I_new - I_old;
-	b_i( 1 ) += I_new - I_old;
+
+	double tmp = g_new - g_old;
+	A_g(0, 0) += tmp;
+	A_g(1, 1) += tmp;
+
+//	A_g(0, 0) = A_g(1, 1) = g_new;
+
+	A_g(0, 1) -= tmp;
+	A_g(1, 0) -= tmp;
+//	A_g(0, 1) = A_g(1, 0) = -g_new;
+
+	tmp = I_new - I_old;
+	b_i(0) -= tmp;
+	b_i(1) += tmp;
+//	b_i(0) = -(b_i(1) = I_new);
 	
 	g_old = g_new;
 	I_old = I_new;
 }
-
-
-
-#ifndef MIN
-# define MIN(x,y) (((x) < (y)) ? (x) : (y))
-#endif
-
-
 
 void Diode::calc_eq()
 {
@@ -113,71 +106,58 @@ void Diode::calc_eq()
 	double v = p_cnode[0]->v - p_cnode[1]->v;
 	
 	// adjust voltage to help convergence
-	if ( V_B != 0 && v < MIN( 0, -V_B + 10 * N ) )
-	{
+	if(V_B != 0 && v < std::min(0.0, 10 * N - V_B ) ) {
 		double V = -(v + V_B);
-		V = diodeVoltage( V, -(V_prev + V_B), N, V_lim );
+		V = diodeVoltage(V, -(V_prev + V_B), N, V_lim);
 		v = -(V + V_B);
-	}
-	else
-		v = diodeVoltage( v, V_prev, N, V_lim );
-	
+	} else	v = diodeVoltage(v, V_prev, N, V_lim);
+
 	V_prev = v;
-	
-	double I_D;
-	calcIg( v, & I_D, & g_new );
-	
-	I_new = I_D - (v * g_new);
+
+	calcIg(v, &I_new, &g_new);
+
+	I_new -= (v * g_new);
 }
 
 
-void Diode::calcIg( double V, double * I_D, double * g ) const
+void Diode::calcIg(double V, double *I_D, double *g) const
 {
 	double I_S = m_diodeSettings.I_S;
-	double N = m_diodeSettings.N;
+	double N   = m_diodeSettings.N;
 	double V_B = m_diodeSettings.V_B;
-// 	double R = m_diodeSettings.R;
+
+	double g_tiny = (V < -10 * V_T * N && V_B != 0) ? I_S : 0;
 	
-	double g_tiny = (V < - 10 * V_T * N && V_B != 0) ? I_S : 0;
-	
-	if ( V >= (-3 * N * V_T) )
-	{
-		if ( g )
-			*g = diodeConductance( V, I_S, N ) + g_tiny;
-		*I_D = diodeCurrent( V, I_S, N ) + (g_tiny * V);
-	}
-	else if ( V_B == 0 || V >= -V_B )
-	{
+	if(V >= (-3 * N * V_T)) {
+		diodeJunction(V, I_S, N, I_D, g);
+		*g += g_tiny;
+		*I_D += g_tiny * V;
+	} else if(V_B == 0 || V >= -V_B) {
 		double a = (3 * N * V_T) / (V * M_E);
 		a = a * a * a;
 		*I_D = (-I_S * (1 + a)) + (g_tiny * V);
-		if ( g )
-			*g = ((I_S * 3 * a) / V) + g_tiny;
-	}
-	else
-	{
-		double a = exp( -(V_B + V) / N / V_T );
+		*g   = ( I_S *  3 * a) / V + g_tiny;
+	} else {
+		double a = exp(-(V_B + V) / N / V_T);
 		*I_D = (-I_S * a) + (g_tiny * V);
-		if ( g )
-			*g = I_S * a / V_T / N + g_tiny;
+		*g   = I_S * a / V_T / N + g_tiny;
 	}
 }
-
 
 void Diode::setDiodeSettings( const DiodeSettings & settings )
 {
 	m_diodeSettings = settings;
 	updateLim();
-	if (p_eSet)
-		p_eSet->setCacheInvalidated();
-}
 
+	if(p_eSet) p_eSet->setCacheInvalidated();
+}
 
 void Diode::updateLim()
 {
 	double I_S = m_diodeSettings.I_S;
 	double N = m_diodeSettings.N;
-	V_lim = diodeLimitedVoltage( I_S, N );
+	V_lim = diodeLimitedVoltage(I_S, N);
 }
+
 //END class Diode
 
