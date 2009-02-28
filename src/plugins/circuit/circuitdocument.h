abdbc93bd0bcae6c889b89e3f6a934c2910b29f9
Julian Bäume <julian@svg4all.de>
2009-02-28 22:17:04 +0100

    re-implement circuitdocument
    still needs some work, to visualize things, but at least KTechLab is now
    able to detect circuit documents and choose the right plugin to load
    them.

---
+++ src/plugins/circuit/circuitdocument.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by Julian Bäume <julian@svg4all.de>                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#ifndef CIRCUITDOCUMENT_H
+#define CIRCUITDOCUMENT_H
+
+#include "shell/partdocument.h"
+
+namespace Sublime
+{
+class Document;
+} // namespace Sublime
+
+namespace KTechLab
+{
+class Core;
+} // namespace KTechLab
+
+/**
+CircuitDocument handles circuit files. It provides visualisation, interaction
+and other information about .circuit files.
+@short Circuit Document
+@author Julian Bäume
+*/
+class CircuitDocument : public KTechLab::PartDocument
+{
+    Q_OBJECT
+public:
+    CircuitDocument( const KUrl &url, KTechLab::Core* core );
+    virtual ~CircuitDocument();
+
+    virtual QString documentType() const;
+
+protected:
+    virtual QWidget *createViewWidget( QWidget* parent = 0 );
+
+};
+
+#endif
+
