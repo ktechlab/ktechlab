dfc920b11c5ccad9433f71a322984320a2381649
Julian Bäume <julian@svg4all.de>
2009-03-01 01:34:35 +0100

    add a component model
    component plugins will register their components within the circuit
    plugin, so users can see the new components in the component browser and
    drag them into circuits.

---
+++ src/plugins/circuit/componentmodel.cpp
@@ -0,0 +1,41 @@
+/***************************************************************************
+ *   Copyright (C) 2008 Julian Bäume <julian@svg4all.de>                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#include "componentmodel.h"
+
+QModelIndex ComponentModel::index( int row, int column, const QModelIndex & parent ) const
+{
+    return parent;
+}
+
+QModelIndex ComponentModel::parent( const QModelIndex & index ) const
+{
+    return index;
+}
+
+int ComponentModel::rowCount( const QModelIndex & parent ) const
+{
+    return 0;
+}
+
+int ComponentModel::columnCount( const QModelIndex & parent ) const
+{
+    Q_UNUSED( parent )
+
+    return 1;
+}
+
+QVariant ComponentModel::data( const QModelIndex & index, int role ) const
+{
+    return QVariant();
+}
+
+
+
+// vim: sw=4 sts=4 et tw=100
