5d5bc372e1890a44c92f6b65c734574ffb1cedeb
Julian Bäume <julian@svg4all.de>
2009-03-07 17:46:16 +0100

    work from the last days..
    * add switches plugin
     * this is going to be the first component plugin, providing a set of
       switches
    * move ComponentModel to interfaces library
    * do changes needed to compile and use everything new in circuit plugin

---
+++ src/interfaces/component/icomponent.h
@@ -0,0 +1,21 @@
+/***************************************************************************
+ *   Copyright (C) 2009 Julian Bäume <julian@svg4all.de>                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#ifndef ICOMPONENT_H
+#define ICOMPONENT_H
+
+#include "../ktlinterfacesexport.h"
+
+class KTLINTERFACES_EXPORT IComponent
+{
+
+};
+
+#endif
+
---
+++ src/plugins/switches/ktlswitchesplugin.h
@@ -0,0 +1,29 @@
+/***************************************************************************
+ *   Copyright (C) 2009 Julian Bäume <julian@svg4all.de>                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#ifndef KTLSWITCHESPLUGIN_H
+#define KTLSWITCHESPLUGIN_H
+
+#include <QVariantList>
+#include "interfaces/component/icomponentplugin.h"
+
+class KTLSwitchesPlugin : public KTechLab::IComponentPlugin
+{
+    Q_OBJECT
+public:
+    KTLSwitchesPlugin( QObject *parent, const QVariantList& args );
+    virtual ~KTLSwitchesPlugin();
+    virtual void unload();
+
+private:
+    void init();
+};
+
+#endif
+
