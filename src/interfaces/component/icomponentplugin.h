033b3e338b3ff6d41088d29438fae0e9d1a3b6e8
Julian Bäume <julian@svg4all.de>
2009-03-07 17:46:16 +0100

    work from the last days..
    * add switches plugin
     * this is going to be the first component plugin, providing a set of
       switches
    * move ComponentModel to interfaces library
    * do changes needed to compile and use everything new in circuit plugin

---
+++ src/interfaces/component/icomponentplugin.h
@@ -0,0 +1,38 @@
+/***************************************************************************
+ *   Copyright (C) 2009 Julian Bäume <julian@svg4all.de>                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#ifndef ICOMPONENTPLUGIN_H
+#define ICOMPONENTPLUGIN_H
+
+#include "../ktlinterfacesexport.h"
+
+#include <interfaces/iplugin.h>
+
+namespace KTechLab
+{
+class IComponent;
+
+class KTLINTERFACES_EXPORT IComponentFactory
+{
+public:
+    virtual ~IComponentFactory() {};
+
+    virtual IComponent * create( const QString &name )=0;
+};
+
+class KTLINTERFACES_EXPORT IComponentPlugin: public KDevelop::IPlugin
+{
+public:
+    IComponentPlugin( KComponentData data, QObject *parent = 0 );
+};
+
+} // namespace KTechLab
+
+#endif
+
