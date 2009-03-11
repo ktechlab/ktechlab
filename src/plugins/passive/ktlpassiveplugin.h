361c9c0b492f6852dd59212814120890d2d14594
Julian Bäume <julian@svg4all.de>
2009-03-11 03:21:56 +0100

    add new plugin for passive components
    for now there is only a resistor

---
+++ src/plugins/passive/ktlpassiveplugin.h
@@ -0,0 +1,33 @@
+/***************************************************************************
+ *   Copyright (C) 2009 Julian Bäume <julian@svg4all.de>                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#ifndef KTLPASSIVEPLUGIN_H
+#define KTLPASSIVEPLUGIN_H
+
+#include <QVariantList>
+#include "interfaces/component/icomponentplugin.h"
+
+class KTLPassiveFactory;
+
+class KTLPassivePlugin : public KTechLab::IComponentPlugin
+{
+    Q_OBJECT
+public:
+    KTLPassivePlugin( QObject *parent, const QVariantList& args );
+    virtual ~KTLPassivePlugin();
+    virtual void unload();
+
+private:
+    void init();
+
+    KTLPassiveFactory * m_componentFactory;
+};
+
+#endif
+
---
+++ src/plugins/passive/resistor.h
@@ -0,0 +1,32 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by John Myers <electronerd@electronerdia.net>      *
+ *   Copyright (C) 2009 by Julian Bäume <julian@svg4all.de>                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#ifndef RESISTOR_H
+#define RESISTOR_H
+
+#include "interfaces/component/icomponent.h"
+
+/**
+ * A standard resistor
+ * \author Julian Bäume
+ */
+class Resistor : public KTechLab::IComponent
+{
+public:
+    Resistor( );
+    virtual ~Resistor();
+
+    static KTechLab::ComponentMetaData metaData();
+
+    virtual void dataChanged();
+
+};
+#endif //RESISTOR_H
+
