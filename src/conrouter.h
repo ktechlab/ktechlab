8078306ca9d6f111aa85ac05263a7131435e1176
AlonzoTG <AlonzoTG@76876f85-f02d-0410-881d-fd707285c4ba>
2009-04-23 00:23:53 +0000

    first pass through conrouter revealed two unused variables, thought it was worthy of a commit.
    git-svn-id: https://ktechlab.svn.sourceforge.net/svnroot/ktechlab/trunk@255 76876f85-f02d-0410-881d-fd707285c4ba

--- src/conrouter.h
+++ src/conrouter.h
@@ -17,6 +17,7 @@
 #include <qvaluelist.h>
 
 class ICNDocument;
+
 class Cell;
 
 typedef QValueList<QPoint> QPointList;
@@ -32,66 +33,76 @@ otherwise indicated).
 
 @author David Saxton
 */
-class ConRouter
-{
+
+class ConRouter {
+
 public:
-	ConRouter( ICNDocument *cv );
+	ConRouter(ICNDocument *cv);
 	~ConRouter();
-	
+
 	/**
 	 * What this class is all about - finding a route, from (sx,sy) to (ex,ey).
 	 */
-	void mapRoute( int sx, int sy, int ex, int ey );
+	void mapRoute(int sx, int sy, int ex, int ey);
+
 	/**
 	 * Translates the precalculated routepoints by the given amount
 	 */
-	void translateRoute( int dx, int dy );
+	void translateRoute(int dx, int dy);
+
 	/**
 	 * Sets the route to the given canvas points
 	 * @param reverse if true, the points in pointList will be reversed
 	 */
-	void setPoints( const QPointList &pointList, bool reverse = false );
+	void setPoints(const QPointList &pointList, bool reverse = false);
+
 	/**
 	 * Sets the route to the given route points
 	 */
-	void setRoutePoints( const QPointList &pointList );
+	void setRoutePoints(const QPointList &pointList);
+
 	/**
 	 * @returns true if the start or end points differ from that of the current route
 	 */
-	bool needsRouting( int sx, int sy, int ex, int ey ) const;
+	bool needsRouting(int sx, int sy, int ex, int ey) const;
+
 	/**
 	 * Returns the list of canvas points
 	 */
-	QPointList pointList( bool reverse ) const;
+	QPointList pointList(bool reverse) const;
+
 	/**
 	 * Returns a pointer to the internall cellPointList
 	 */
-	QPointList *cellPointList() { return &m_cellPointList; }
+	QPointList *cellPointList() {
+		return &m_cellPointList;
+	}
+
 	/**
 	 * This will return two lists of Canvas points from the splitting of the
 	 * route at the Canvas point "pos". The internall stored points are not
 	 * affected.
 	 */
-	QPointListList splitPoints( const QPoint &pos ) const;
+	QPointListList splitPoints(const QPoint &pos) const;
+
 	/**
 	 * This will return a list of Canvas pointLists from the route, divided
 	 * into n parts (at n-1 equally spaced places).
 	 */
-	QPointListList dividePoints( uint n ) const;
+	QPointListList dividePoints(uint n) const;
 
 protected:
 	/**
 	 * Check a line of the ICNDocument cells for a valid route
 	 */
-	bool checkLineRoute( int scx, int scy, int ecx, int ecy, int maxConScore, int maxCIScore );
-	void checkACell( int x, int y, Cell *prev, int prevX, int prevY, int nextScore );
-	void checkCell( int x, int y ); // Gets the shortest route from the final cell
+	bool checkLineRoute(int scx, int scy, int ecx, int ecy, int maxConScore, int maxCIScore);
+	void checkACell(int x, int y, Cell *prev, int prevX, int prevY, int nextScore);
+	void checkCell(int x, int y);   // Gets the shortest route from the final cell
 	/**
 	 * Remove duplicated points from the route
 	 */
 	void removeDuplicatePoints();
-	
-	int xcells, ycells;
+
 	int m_lcx, m_lcy; // Last x / y from mapRoute, if we need a point on the route
 	Cells *cellsPtr;
 	TempLabelMap tempLabels;
