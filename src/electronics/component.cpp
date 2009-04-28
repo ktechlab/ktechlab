7c33660a99bd907a799dce48006f9d18bab4af87
Alan Grimes <AlonzoTG@users.sf.net>
2009-04-28 23:24:09 +0000

    Moved two static methods out of component and turned them into proper functions.
    git-svn-id: https://ktechlab.svn.sourceforge.net/svnroot/ktechlab/trunk@275 76876f85-f02d-0410-881d-fd707285c4ba

--- src/electronics/component.cpp
+++ src/electronics/component.cpp
@@ -538,27 +538,6 @@ void Component::initElements(const uint stage) {
 ECNode *Component::createPin(double x, double y, int orientation, const QString &name) {
 	return dynamic_cast<ECNode*>(createNode(x, y, orientation, name, Node::ec_pin));
 }
-
-// static
-double Component::voltageLength(double v) {
-	double v_max = 1e1;
-	double v_min = 1e-1;
-
-	v = std::abs(v);
-
-	if(v >= v_max) return 1.0;
-	else if(v <= v_min) return 0.0;
-	else return std::log(v / v_min) / std::log(v_max / v_min);
-}
-
-// static
-QColor Component::voltageColor(double v) {
-	double prop = voltageLength(v);
-
-	if(v >= 0)
-		return QColor(int(255 * prop), int(166 * prop), 0);
-	else return QColor(0, int(136 * prop), int(255 * prop));
-}
 //END class Component
 
 #include "component.moc"
