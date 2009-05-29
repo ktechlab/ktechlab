1a1ae113e32d2e1b7c540541b450d6034d8a7833
Alan Grimes <AlonzoTG@users.sf.net>
2009-05-29 03:08:08 +0000

    N needs to be accessable as a signed.
    git-svn-id: https://ktechlab.svn.sourceforge.net/svnroot/ktechlab/trunk@371 76876f85-f02d-0410-881d-fd707285c4ba

--- src/electronics/simulation/element.h
+++ src/electronics/simulation/element.h
@@ -32,11 +32,11 @@ extern double T_K; ///< Temperature in Kelvin
 class CNode
 {
 public:
-	CNode() : v(0.0), i(0.0), m_n(0) {}
-	CNode(const int32_t n) : v(0.0), i(0.0), m_n(n) {}
+	CNode() : i(0.0), v(0.0), m_n(0) {}
+	CNode(const int32_t n) : i(0.0), v(0.0), m_n(n) {}
 
-	inline void set_n(const uint32_t n) { m_n = n; }
-	inline uint32_t n() const { return m_n; }
+	inline void set_n(const int32_t n) { m_n = n; }
+	inline int32_t n() const { return m_n; }
 
 	inline double voltage() const { return v; }
 	inline void setVoltage(double volts) { if(m_n > -1) v = volts; }
@@ -44,13 +44,17 @@ public:
 	inline bool isGround() const { return m_n == -1; }
 	void setGround(); 
 
+	inline void resetCurrent() { i = 0.0; }
+	inline void sinkCurrent(const double current) { i -= current; } // classical currents 
+	inline void sourceCurrent(const double current) { i += current; }
 	inline double current() const { return i; }
 	inline void setCurrent(const double ampres) { i = ampres; }
 
 private:
 	/// Voltage on node. This is set from the last calculated voltage.
-	double v;
-	double i; // gonna refactor this from element.
+	/// current is a read-out varriable. 
+	double i, v;
+// FIXME/WARNING: current won't be correct when Cnode is shared between elements! =( 
 
 	/// CNode number
 	int32_t m_n;
