ff60a37411dfe44332ccbaf8a914df01b8cdf2fe
Julian Bäume <julian@svg4all.de>
2009-06-26 15:08:46 +0200

    first work on interactive components
    this doesn't work, so please don't try to compile ;)

---
+++ src/interfaces/component/interactivecomponent.h
@@ -0,0 +1,22 @@
+/***************************************************************************
+ *   Copyright (C) 2009 Julian Bäume <julian@svg4all.de>                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#ifndef INTERACTIVECOMPONENT_H
+#define INTERACTIVECOMPONENT_H
+
+#include <Plasma/Svg>
+#include "../ktlinterfacesexport.h"
+
+class KTLINTERFACES_EXPORT InteractiveComponent: public Plasma::Svg
+{
+public:
+    InteractiveComponent();
+};
+
+#endif // INTERACTIVECOMPONENT_H
