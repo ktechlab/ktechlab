cc322d7f95fffef884bc038fa251959f221830c9
Zoltan Padrah <zoltan_padrah@users.sf.net>
2009-07-19 21:38:12 +0000

    added documentation comments
    git-svn-id: https://ktechlab.svn.sourceforge.net/svnroot/ktechlab/trunk@424 76876f85-f02d-0410-881d-fd707285c4ba

--- src/math/matrix.h
+++ src/math/matrix.h
@@ -36,44 +36,58 @@ substitution, and a few other useful operations. Steps in using class:
 class Matrix
 {
 public:
-        /**
+       /**
         * Creates a size x size square matrix m, with all values zero,
         * and a right side vector x of size m+n
         */
         Matrix(CUI size);
 
-        /**
+       /**
         * Destructor
         */
         ~Matrix();
 
-        /**
+       /**
         * Returns true if the matrix is changed since last calling performLU()
         * - i.e. if we do need to call performLU again.
         */
         inline bool isChanged() const { return max_k < m_mat->size_m(); }
-        /**
+
+       /**
         * Performs LU decomposition. Going along the rows,
         * the value of the decomposed LU matrix depends only on
         * the previous values.
         */
         void performLU();
-        /**
+       /**
         * Applies the right side vector (x) to the decomposed matrix,
         * with the solution returned in x.
         */
         void fbSub(QuickVector *x);
-        /**
-        * Prints the matrix to stdout
+
+       /**
+        * Prints the matrix to an output stream
+        * @param outstream the stream to print to
         */
         void displayMatrix(std::ostream &outstream) const;
+
         /**
-        * Prints the LU-decomposed matrix to stdout
+        * Prints the LU-decomposed matrix to an output stream
+         * @param outstream the stream to print to
         */
         void displayLU(std::ostream &outstream) const;
 
+        /**
+         * Set the matrix to identity
+         */
         void identity() { *m_mat = 1; max_k = 0; }
 
+        /**
+         * @todo DOCUMENT THIS
+         * @param i 
+         * @param j 
+         * @return 
+         */
         double &g(CUI i, CUI j) {
                 const unsigned int mapped_i = m_inMap[i];
                 if(mapped_i < max_k) max_k = mapped_i;
@@ -86,16 +100,40 @@ public:
                 return(*m_mat)[mapped_i][j];
         }
 
+        /**
+         * @todo DOCUMENT THIS 2
+         * @param i 
+         * @param j 
+         * @return 
+         */
         double  g(CUI i, CUI j) const { return m_mat->at(m_inMap[i], j); }
 
         /**
         * Multiplies this matrix by the Vector rhs, and places the result
         * in the vector pointed to by result. Will fail if wrong size.
-        */
+         * @param rhs vector to multiply the vector with
+         * @param result matrix * rhs, vector @todo check if the order is right
+         */
         void multiply(const QuickVector *rhs, QuickVector *result );
+
+        /**
+         * \fn Matrix::validateLU()
+         * check the validity of LU factorization.
+         * It's an expensive procedure only for debugging.
+         * @return sum of absolute error between the original matrix and the decomposition
+         * 
+         * CAUTION: SUSPECT BUGS IN THIS FUNCTION JUST AS QUICKLY AS BUGS IN THE ABOVE. 
+         */
         double validateLU() const;
 
-    bool validate() const;
+        /**
+         * @fn Matrix::validate()
+         * If this here function fails, the matrix is no good and 
+         * no further computation should be done. This method should be used before performLU.
+         * @return true, if the matrix is valid, false otherwise
+         */ 
+        bool validate() const;
+
 private:
         /**
         * Swaps around the rows in the (a) the matrix; and (b) the mappings
