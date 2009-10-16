e48542d5fceca10f130e1dc4e058a79c16dcf017
Alan Grimes <AlonzoTG@users.sf.net>
2009-10-16 02:58:44 +0000

    git-svn-id: https://ktechlab.svn.sourceforge.net/svnroot/ktechlab/trunk@521 76876f85-f02d-0410-881d-fd707285c4ba

--- src/electronics/components/ecvoltagesource.cpp
+++ src/electronics/components/ecvoltagesource.cpp
@@ -18,75 +18,67 @@
 #include <klocale.h>
 #include <qpainter.h>
 
-Item* ECCell::construct( ItemDocument *itemDocument, bool newItem, const char *id )
-{
-	return new ECCell( (ICNDocument*)itemDocument, newItem, id );
+Item* ECCell::construct(ItemDocument *itemDocument, bool newItem, const char *id) {
+    return new ECCell((ICNDocument*)itemDocument, newItem, id);
 }
 
-LibraryItem* ECCell::libraryItem()
-{
-	QStringList ids;
-	ids << "ec/battery" << "ec/cell";
-	return new LibraryItem(
-		ids,
-		i18n("Battery"),
-		i18n("Sources"),
-		"cell.png",
-		LibraryItem::lit_component,
-		ECCell::construct);
+LibraryItem* ECCell::libraryItem() {
+    QStringList ids;
+    ids << "ec/battery" << "ec/cell";
+    return new LibraryItem(
+               ids,
+               i18n("Battery"),
+               i18n("Sources"),
+               "cell.png",
+               LibraryItem::lit_component,
+               ECCell::construct);
 }
 
 ECCell::ECCell(ICNDocument *icnDocument, bool newItem, const char *id)
-	: SimpleComponent(icnDocument, newItem, id ? id : "cell")
-{
-	m_name = i18n("Battery");
-	setSize(-8, -8, 16, 16);
-	voltage = 0;
+        : SimpleComponent(icnDocument, newItem, id ? id : "cell") {
+    m_name = i18n("Battery");
+    setSize(-8, -8, 16, 16);
 
-	init1PinLeft();
-	init1PinRight();
+    init1PinLeft();
+    init1PinRight();
 
-	m_pNNode[0]->pin()->setGroundType(Pin::gt_medium);
-//	m_voltageSource = createVoltageSource(m_pNNode[0]->pin(), m_pPNode[0]->pin(), voltage);
-	m_voltageSource = new VoltageSource(voltage);
-	setup2pinElement(m_voltageSource, m_pNNode[0]->pin(), m_pPNode[0]->pin());
+    m_pNNode[0]->pin()->setGroundType(Pin::gt_medium);
 
-	createProperty("voltage", Variant::Type::Double);
-	property("voltage")->setUnit("V");
-	property("voltage")->setCaption(i18n("Voltage"));
-	property("voltage")->setMinValue(-1e12);
-	property("voltage")->setMaxValue(1e12);
-	property("voltage")->setValue(5.0);
+    setup2pinElement(&m_voltageSource, m_pNNode[0]->pin(), m_pPNode[0]->pin());
 
-	addDisplayText("voltage", QRect(-16, -24, 32, 16), "");
+    createProperty("voltage", Variant::Type::Double);
+    property("voltage")->setUnit("V");
+    property("voltage")->setCaption(i18n("Voltage"));
+    property("voltage")->setMinValue(-1e12);
+    property("voltage")->setMaxValue(1e12);
+    property("voltage")->setValue(5.0);
+
+    addDisplayText("voltage", QRect(-16, -24, 32, 16), "");
 }
 
-ECCell::~ECCell()
-{
+ECCell::~ECCell() {
 }
 
-void ECCell::dataChanged()
-{
-	voltage = dataDouble("voltage");
-	m_voltageSource->setVoltage(voltage);
-	
-	QString display = QString::number( voltage / getMultiplier(voltage), 'g', 3 ) + getNumberMag(voltage) + "V";
-	setDisplayText( "voltage", display );
+void ECCell::dataChanged() {
+    const double voltage = dataDouble("voltage");
+
+    m_voltageSource.setVoltage(voltage);
+    QString display = QString::number(voltage / getMultiplier(voltage), 'g', 3) + getNumberMag(voltage) + "V";
+    setDisplayText("voltage", display);
 }
 
-void ECCell::drawShape( QPainter &p )
-{
-	initPainter(p);
-	
-	int _x = (int)x()-8;
-	int _y = (int)y()-24;
-	
-	p.drawLine( _x,		_y+20,	_x,	_y+28 );
-	p.drawLine( _x+5,	_y+16,	_x+5,	_y+32 );
-	p.drawLine( _x+10,	_y+20,	_x+10,	_y+28 );
-	p.drawLine( _x+15,	_y+16,	_x+15,	_y+32 );
-	
-	deinitPainter(p);
-// 	p.drawPolyline( areaPoints() );
+void ECCell::drawShape(QPainter &p) {
+    initPainter(p);
+
+    int _x = (int)x() - 8;
+    int _y = (int)y() - 24;
+
+    p.drawLine(_x,  _y + 20, _x, _y + 28);
+    p.drawLine(_x + 5, _y + 16, _x + 5, _y + 32);
+    p.drawLine(_x + 10, _y + 20, _x + 10, _y + 28);
+    p.drawLine(_x + 15, _y + 16, _x + 15, _y + 32);
+
+    deinitPainter(p);
+//  p.drawPolyline( areaPoints() );
 }
 
