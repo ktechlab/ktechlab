4578ce6b3d01b26ba06be3a781ffea6c94a9a205
Julian Bäume <julian@svg4all.de>
2009-12-05 02:51:51 +0100

    rename CircuitApplet -> CircuitScene
    applet is an old name used in plasma, doesn't make much sense here.

---
+++ src/plugins/circuit/circuitscene.cpp
@@ -0,0 +1,113 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+#include "circuitscene.h"
+#include "theme.h"
+#include "interfaces/component/componentmimedata.h"
+
+#include <QGraphicsSceneDragDropEvent>
+#include <KDebug>
+
+using namespace KTechLab;
+
+
+CircuitScene::CircuitScene ( QObject* parent, const QVariantList& args )
+ : QGraphicsScene ( parent ),
+   m_theme( new Theme() )
+{
+    foreach ( QVariant arg, args ) {
+        if ( arg.canConvert(QVariant::Map) && arg.toMap().contains( "circuitName" ) ) {
+            setCircuitName( arg.toMap().value( "circuitName" ).toString() );
+        }
+    }
+    m_theme->setThemeName( "default" );
+//    KConfigGroup cg = config( "circuit" );
+//    m_componentTheme = cg.readEntry( "componentTheme", "din" );
+
+//    m_componentSize = QSizeF( cg.readEntry("componentWidth", "64").toInt(), cg.readEntry("componentHeight", "64").toInt() );
+}
+
+
+CircuitScene::~CircuitScene()
+{
+    qDeleteAll( m_components.values() );
+}
+
+QString CircuitScene::circuitName() const
+{
+
+}
+
+void CircuitScene::dropEvent ( QGraphicsSceneDragDropEvent* event )
+{
+    if (!event->mimeData()->hasFormat("application/x-icomponent")) {
+        return;
+    }
+    const ComponentMimeData *mimeData = qobject_cast<const ComponentMimeData*>(event->mimeData());
+
+    //FIXME: implement me!
+    //do something with mimeData here. it should be added to the document using the document
+    //DataEngine and services. use mimeData->createComponent() to create a new component.
+    kDebug() << "Dropping item @"<< event->scenePos() << "type:" << mimeData->data("application/x-icomponent");
+}
+
+void CircuitScene::setupData()
+{
+    //Plasma::DataEngine *docEngine = dataEngine( "ktechlabdocument" );
+//    if ( !docEngine ) {
+        kWarning() << "No document engine found" << endl;
+        return;
+//    }
+
+    //docEngine->connectSource( m_circuitName, this );
+}
+
+void CircuitScene::dataUpdated( const QString &name, const QVariantList &data )
+{
+    //Plasma::DataEngine *docEngine = dataEngine( "ktechlabdocument" );
+    //kDebug() << "isContainment() ==" << isContainment();
+/*
+    if ( data["mime"].toString().endsWith("circuit") ) {
+        const QStringList &itemSources = data["itemList"].toStringList();
+        foreach ( const QString &source, itemSources ) {
+            if ( !m_components.contains(source) ) {
+                ComponentApplet *component = new ComponentApplet( this, m_theme );
+                kDebug() << "before connecting";
+                //docEngine->connectSource( m_circuitName + "/" + source, component );
+                kDebug() << "after connecting";
+
+                //addApplet( component, position, false );
+                m_components.insert( source, component );
+            }
+        }
+    }*/
+    //kDebug() << "Difference between components and applets" << (m_components.size() - applets().size());
+}
+
+void KTechLab::CircuitScene::setCircuitName ( const QString& name )
+{
+    //if name didn't change, do nothing
+    if ( m_circuitName == name ) {
+        return;
+    }
+
+    m_circuitName = name;
+    setupData();
+}
