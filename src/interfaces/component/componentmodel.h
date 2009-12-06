bfaed3080b7451e160ad318c3dc30411e8a3a86d
Julian Bäume <julian@svg4all.de>
2009-12-06 03:13:29 +0100

    move class definition into cpp file
    don't define a class, only used by ComponentModel in the header file. If
    others include the header, they won't include the class definition any
    longer.

--- src/interfaces/component/componentmodel.h
+++ src/interfaces/component/componentmodel.h
@@ -20,37 +20,7 @@ namespace KTechLab
 {
 
 class IComponentFactory;
-
-class ComponentItem
-{
-public:
-    ComponentItem();
-    ~ComponentItem();
-
-    void addChild( ComponentItem * child );
-    void setParent( ComponentItem * parent );
-    ComponentItem * parent();
-
-    ComponentItem * child( int row );
-    ComponentItem * child( const QString &key );
-
-    void setMetaData( const ComponentMetaData & data );
-    void setFactory( IComponentFactory * factory );
-
-    int row();
-    int rowCount();
-    QList<ComponentItem*> children();
-
-    ComponentMetaData metaData() const;
-    IComponentFactory * factory() const;
-
-private:
-    IComponentFactory * m_factory;
-    ComponentMetaData m_metaData;
-
-    QMultiMap<QString,ComponentItem*> m_children;
-    ComponentItem * m_parent;
-};
+class ComponentItem;
 
 /**
  * This class implements QAbstractItemModel to provide a tree of compoments sorted into groups like
