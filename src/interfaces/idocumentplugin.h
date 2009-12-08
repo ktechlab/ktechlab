8054f9a20ba0643a5aa4f3b8ba8b680f065309e2
Julian Bäume <julian@svg4all.de>
2009-12-08 02:17:36 +0100

    add fileNameForComponent() method to IDocumentPlugin
    also add documentation for IDocumentPlugin and provide a default
    implementation

--- src/interfaces/idocumentplugin.h
+++ src/interfaces/idocumentplugin.h
@@ -27,6 +27,14 @@ namespace KTechLab
 
 class IComponentFactory;
 
+/**
+ * \short Base-class for Plugins handling documents
+ * Sub-class this interface to support a new document-class for
+ * KTechLab. KTechLab documents are composed of different components.
+ * This interface unifies this and provides basic handling of these components.
+ *
+ * \author Julian Bäume
+ */
 class KTLINTERFACES_EXPORT IDocumentPlugin: public KDevelop::IPlugin
 {
     Q_OBJECT
@@ -36,14 +44,34 @@ public:
 
     /**
      * create a DataContainer for a given document
+     * FIXME:provide better documentation
      */
     virtual DataContainer * createDataContainer( KDevelop::IDocument *document, const QString &component = QString() )=0;
 
     /**
      * create a DataContainer for a given component
+     * FIXME:provide better documentation
      */
     DataContainer * createComponentContainer( const QString &component );
+    /**
+     * Find the name of the file containing the graphical representation of
+     * the given component. The default implementation will simply normalize the
+     * components name to contain no slashes and add .svgz as the default suffix.
+     * You are encouraged to override this method in your own implementation to support
+     * multiple components per file.
+     *
+     * \param component - the name of the component
+     * \returns the filename containing the component
+     */
+    virtual QString fileNameForComponent( const QString &component );
 
+    /**
+     * Each component factory can register itself at the document plugin, to make
+     * it aware of it's presence. It will then determine which components the
+     * factory has to offer and can handle them accordingly.
+     *
+     * \param factory - the component factory
+     */
     virtual void registerComponentFactory( KTechLab::IComponentFactory * factory )=0;
 };
 
