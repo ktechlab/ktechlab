daacd3e5cfabb329d8c3ab6502deb996b751fa5d
Julian Bäume <julian@svg4all.de>
2010-01-04 20:06:39 +0100

    Adding initial FlowConnectorList implementation
    git-svn-id: https://ktechlab.svn.sourceforge.net/svnroot/ktechlab/trunk@165 76876f85-f02d-0410-881d-fd707285c4ba
    
    Conflicts:
    
    	src/flowparts/Makefile.am

---
+++ src/flowparts/flowconnectorlist.cpp
@@ -0,0 +1,15 @@
+//
+// C++ Implementation: flowconnectorlist
+//
+// Description: 
+//
+//
+// Author: David Saxton, Alan Grimes, Zoltan Padrah <zoltan.padrah@gmail.com>, (C) 2009
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include <qguardedptr.h>
+#include <qvaluelist.h>
+
+#include "flowconnectorlist.h"
