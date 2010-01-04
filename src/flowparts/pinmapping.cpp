89ea35b467f22ea81f1c00ed1a5f749f88563c55
Julian Bäume <julian@svg4all.de>
2010-01-04 19:59:18 +0100

    cherry-pick from svn trunk
    original message:
    
    Lets push ahead... (fixes some of the problem exposed by previous illconsidered commit...)
    
    git-svn-id: https://ktechlab.svn.sourceforge.net/svnroot/ktechlab/trunk@152 76876f85-f02d-0410-881d-fd707285c4ba
    
    Conflicts:
    
    	src/Makefile.am
    	src/connector.cpp
    	src/docmanager.cpp
    	src/itemlibrary.cpp
    	src/math/Makefile.am

--- src/flowparts/pinmapping.cpp
+++ src/flowparts/pinmapping.cpp
@@ -121,7 +121,7 @@ void PinMapEditor::savePinMapping()
 
 //BEGIN class PinMapDocument
 PinMapDocument::PinMapDocument()
-	: CircuitICNDocument( 0, 0 )
+	: FlowICNDocument( 0, 0 )
 {
 	m_pPicComponent = 0l;
 	m_pKeypad = 0l;
