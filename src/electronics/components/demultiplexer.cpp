4f3687933b36e3a504f383b7004c19593aeb0f80
Julian Bäume <julian@svg4all.de>
2010-01-05 14:51:40 +0100

    memory leaks fixed.
    git-svn-id: https://ktechlab.svn.sourceforge.net/svnroot/ktechlab/trunk@326 76876f85-f02d-0410-881d-fd707285c4ba
    
    Conflicts:
    
    	src/electronics/components/ecclockinput.cpp

--- src/electronics/components/demultiplexer.cpp
+++ src/electronics/components/demultiplexer.cpp
@@ -8,123 +8,125 @@
  *   (at your option) any later version.                                   *
  ***************************************************************************/
 
-#include "demultiplexer.h"
-
-#include "logic.h"
-#include "libraryitem.h"
+#include <cmath>
 
 #include <kiconloader.h>
 #include <klocale.h>
 
-#include <cmath>
+#include "demultiplexer.h"
+#include "logic.h"
+#include "libraryitem.h"
 
-Item* Demultiplexer::construct( ItemDocument *itemDocument, bool newItem, const char *id )
-{
-	return new Demultiplexer( (ICNDocument*)itemDocument, newItem, id );
+Item* Demultiplexer::construct(ItemDocument *itemDocument, bool newItem, const char *id) {
+	return new Demultiplexer((ICNDocument*)itemDocument, newItem, id);
 }
 
-LibraryItem* Demultiplexer::libraryItem()
-{
+LibraryItem* Demultiplexer::libraryItem() {
 	return new LibraryItem(
-		"ec/demultiplexer",
-		i18n("Demultiplexer"),
-		i18n("Integrated Circuits"),
-		"ic1.png",
-		LibraryItem::lit_component,
-		Demultiplexer::construct
-			);
+	           "ec/demultiplexer",
+	           i18n("Demultiplexer"),
+	           i18n("Integrated Circuits"),
+	           "ic1.png",
+	           LibraryItem::lit_component,
+	           Demultiplexer::construct);
 }
 
-Demultiplexer::Demultiplexer( ICNDocument *icnDocument, bool newItem, const char *id )
-	: DIPComponent( icnDocument, newItem, id ? id : "demultiplexer" )
-{
+Demultiplexer::Demultiplexer(ICNDocument *icnDocument, bool newItem, const char *id)
+		: DIPComponent(icnDocument, newItem, id ? id : "demultiplexer") {
 	m_name = i18n("Demultiplexer");
-	
-	m_input = 0l;
-	
-	createProperty( "addressSize", Variant::Type::Int );
-	property("addressSize")->setCaption( i18n("Address Size") );
+	m_input = 0;
+
+	createProperty("addressSize", Variant::Type::Int);
+	property("addressSize")->setCaption(i18n("Address Size"));
 	property("addressSize")->setMinValue(1);
 	property("addressSize")->setMaxValue(8);
 	property("addressSize")->setValue(1);
-	
+
 	// For backwards compatibility
-	createProperty( "numInput", Variant::Type::Int );
+	createProperty("numInput", Variant::Type::Int);
 	property("numInput")->setMinValue(-1);
 	property("numInput")->setValue(-1);
 	property("numInput")->setHidden(true);
 }
 
-Demultiplexer::~Demultiplexer()
-{
+Demultiplexer::~Demultiplexer() {
+
+	if (m_input) {
+		delete m_input;
+	}
+
+	for (unsigned i = 0; i < m_aLogic.size(); ++i) {
+		delete m_aLogic[i];
+	}
+
+	for (unsigned i = 0; i < m_xLogic.size(); ++i)
+		delete m_xLogic[i];
 }
 
-
-void Demultiplexer::dataChanged()
-{
-	if ( hasProperty("numInput") && dataInt("numInput") != -1 )
-	{
-		int addressSize = int( std::ceil( std::log( (double)dataInt("numInput") ) / std::log(2.0) ) );
+void Demultiplexer::dataChanged() {
+	if (hasProperty("numInput") && dataInt("numInput") != -1) {
+		int addressSize = int(std::ceil(std::log((double)dataInt("numInput")) / std::log(2.0)));
 		property("numInput")->setValue(-1);
-		
-		if ( addressSize < 1 )
+
+		if (addressSize < 1)
 			addressSize = 1;
-		else if ( addressSize > 8 )
+		else if (addressSize > 8)
 			addressSize = 8;
-		
+
 		// This function will get called again when we set the value of numInput
 		property("addressSize")->setValue(addressSize);
+
 		return;
 	}
-	
-	if ( hasProperty("numInput") )
-	{
+
+	if (hasProperty("numInput")) {
 		m_variantData["numInput"]->deleteLater();
 		m_variantData.remove("numInput");
 	}
-	
-	initPins( unsigned(dataInt("addressSize")) );
+
+	initPins(unsigned(dataInt("addressSize")));
 }
 
-
-void Demultiplexer::inStateChanged( bool /*state*/ )
-{
+void Demultiplexer::inStateChanged(bool /*state*/) {
 	unsigned long long pos = 0;
-	for ( unsigned i = 0; i < m_aLogic.size(); ++i )
-	{
-		if ( m_aLogic[i]->isHigh() )
+
+	for (unsigned i = 0; i < m_aLogic.size(); ++i) {
+		if (m_aLogic[i]->isHigh())
 			pos += 1 << i;
 	}
-	for ( unsigned i = 0; i < m_xLogic.size(); ++i )
-		m_xLogic[i]->setHigh( (pos == i) && m_input->isHigh() );
+
+	for (unsigned i = 0; i < m_xLogic.size(); ++i)
+		m_xLogic[i]->setHigh((pos == i) && m_input->isHigh());
 }
 
-
-void Demultiplexer::initPins( unsigned newAddressSize )
-{
+void Demultiplexer::initPins(unsigned newAddressSize) {
 	unsigned oldAddressSize = m_aLogic.size();
 	unsigned long long oldXLogicCount = m_xLogic.size();
 	unsigned long long newXLogicCount = 1 << newAddressSize;
-	
-	if ( newXLogicCount == oldXLogicCount )
+
+	if (newXLogicCount == oldXLogicCount)
 		return;
-	
+
 	QStringList pins;
-	
-	for ( unsigned i=0; i<newAddressSize; ++i )
+
+	for (unsigned i = 0; i < newAddressSize; ++i)
 		pins += "A" + QString::number(i);
-	for(unsigned i = newAddressSize; i < (newXLogicCount + (newXLogicCount % 2)) / 2; ++i)
+
+	for (unsigned i = newAddressSize; i < (newXLogicCount + (newXLogicCount % 2)) / 2; ++i)
 		pins += "";
+
 	pins += "X";
-	for(unsigned i = (newXLogicCount+(newXLogicCount % 2)) / 2 + 1; i<newXLogicCount; ++i)
+
+	for (unsigned i = (newXLogicCount + (newXLogicCount % 2)) / 2 + 1; i < newXLogicCount; ++i)
 		pins += "";
-	for(int i = newXLogicCount - 1; i >= 0; --i)
+
+	for (int i = newXLogicCount - 1; i >= 0; --i)
 		pins += "X" + QString::number(i);
 
 	initDIPSymbol(pins, 64);
 	initDIP(pins);
 
-	if(!m_input) {
+	if (!m_input) {
 
 		m_input = new LogicIn(LogicIn::getConfig());
 		setup1pinElement(m_input, ecNodeWithID("X")->pin());
@@ -132,9 +134,9 @@ void Demultiplexer::initPins( unsigned newAddressSize )
 		m_input->setCallback(this, (CallbackPtr)(&Demultiplexer::inStateChanged));
 	}
 
-	if(newXLogicCount > oldXLogicCount) {
+	if (newXLogicCount > oldXLogicCount) {
 		m_xLogic.resize(newXLogicCount);
-		for(unsigned i = oldXLogicCount; i < newXLogicCount; ++i) {
+		for (unsigned i = oldXLogicCount; i < newXLogicCount; ++i) {
 
 			LogicOut *outLogic = new LogicOut(LogicIn::getConfig(), false);
 			setup1pinElement(outLogic, ecNodeWithID("X" + QString::number(i))->pin());
@@ -142,7 +144,7 @@ void Demultiplexer::initPins( unsigned newAddressSize )
 		}
 
 		m_aLogic.resize(newAddressSize);
-		for(unsigned i = oldAddressSize; i < newAddressSize; ++i) {
+		for (unsigned i = oldAddressSize; i < newAddressSize; ++i) {
 			// node = ecNodeWithID("A" + QString::number(i));
 
 			LogicIn *inLogic = new LogicIn(LogicIn::getConfig());
@@ -152,22 +154,22 @@ void Demultiplexer::initPins( unsigned newAddressSize )
 			m_aLogic[i]->setCallback(this, (CallbackPtr)(&Demultiplexer::inStateChanged));
 		}
 	} else {
-		for ( unsigned i = newXLogicCount; i < oldXLogicCount; ++i )
-		{
+		for (unsigned i = newXLogicCount; i < oldXLogicCount; ++i) {
 			QString id = "X" + QString::number(i);
 			removeDisplayText(id);
-			removeElement( m_xLogic[i], false );
+			removeElement(m_xLogic[i], false);
 			removeNode(id);
 		}
+
 		m_xLogic.resize(newXLogicCount);
 
-		for ( unsigned i = newAddressSize; i < oldAddressSize; ++i )
-		{
+		for (unsigned i = newAddressSize; i < oldAddressSize; ++i) {
 			QString id = "A" + QString::number(i);
 			removeDisplayText(id);
-			removeElement( m_aLogic[i], false );
+			removeElement(m_aLogic[i], false);
 			removeNode(id);
 		}
+
 		m_aLogic.resize(newAddressSize);
 	}
 }
