d53eacd8fc9fac6cb75e92f77a071041fe08f47f
Julian Bäume <julian@svg4all.de>
2010-01-05 14:15:19 +0100

    extracted CanvasTip from itemdocument.* and cleaned up includes to itemdocument.
    git-svn-id: https://ktechlab.svn.sourceforge.net/svnroot/ktechlab/trunk@267 76876f85-f02d-0410-881d-fd707285c4ba
    
    Conflicts:
    
    	src/Makefile.am
    	src/connector.cpp
    	src/itemdocument.cpp
    	src/itemdocument.h
    	src/itemview.cpp

---
+++ src/canvastip.h
@@ -0,0 +1,49 @@
+//
+// C++ Interface: canvastip
+//
+// Description:
+//
+//
+// Author: David Saxton, Alan Grimes <agrimes@speakeasy.net>, (C) 2009
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#ifndef CANVASTIP_H
+#define CANVASTIP_H
+
+#include <canvas.h>
+#include <qvaluevector.h>
+
+class ItemDocument;
+class ECNode;
+class Connector;
+
+/**
+@author David Saxton
+*/
+
+class CanvasTip : public QCanvasRectangle {
+
+public:
+	CanvasTip(ItemDocument *itemDocument, QCanvas *qcanvas);
+	virtual ~CanvasTip();
+
+	void displayVI(ECNode *node, const QPoint &pos);
+	void displayVI(Connector *connector, const QPoint &pos);
+
+protected:
+	virtual void draw(QPainter &p);
+	void setText(const QString & text);
+	bool updateVI();
+	void display(const QPoint &pos);
+	QString displayText(unsigned num) const;
+
+	QValueVector<double> m_v;
+	QValueVector<double> m_i;
+	ItemDocument *p_itemDocument;
+	QString m_text;
+};
+
+#endif
