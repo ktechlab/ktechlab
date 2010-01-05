711ee194c97c1a647797db7461540a2e7c8fdda2
Julian Bäume <julian@svg4all.de>
2010-01-05 14:29:15 +0100

    found a dead helper class in icndocument.h
    git-svn-id: https://ktechlab.svn.sourceforge.net/svnroot/ktechlab/trunk@277 76876f85-f02d-0410-881d-fd707285c4ba
    
    Conflicts:
    
    	src/cells.cpp
    	src/cells.h
    	src/conrouter.cpp
    	src/conrouter.h
    	src/icndocument.h

--- src/utils.h
+++ src/utils.h
@@ -3,80 +3,65 @@
 #define UTILS_H
 
 #include <qpoint.h>
-
-//#include <cmath>
 #include <math.h>
 
-inline int roundDown(const int x, const int roundness)
-{
-// this code rounds down, but the program doesn't work if we use this version. 
+inline int roundDown(const int x, const int roundness) {
+// this code rounds down, but the program doesn't work if we use this version.
 //	return x - (x % roundness);
 
-//FIXME: What is this function really supposed to do? 
-// must be important because it's called millions of times! 
-	if(x < 0)
+//FIXME: What is this function really supposed to do?
+// must be important because it's called millions of times!
+	if (x < 0)
 		return (x - roundness + 1) / roundness;
 	else	return (x / roundness);
 }
 
-// WHAT THE HELL IS THIS CODE SUPPOSED TO DO? 
-inline int roundDown( double x, int roundness )
-{
-	return roundDown( int(x), roundness );
+// WHAT THE HELL IS THIS CODE SUPPOSED TO DO?
+inline int roundDown(double x, int roundness) {
+	return roundDown(int(x), roundness);
 }
 
-inline QPoint roundDown( const QPoint & p, int roundness )
-{
-	return QPoint( roundDown( p.x(), roundness ), roundDown( p.y(), roundness ) );
+inline QPoint roundDown(const QPoint &p, int roundness) {
+	return QPoint(roundDown(p.x(), roundness), roundDown(p.y(), roundness));
 }
 
-inline int toCanvas( int pos )
-{
-    return (pos << 3) + 4;
+inline int toCanvas(int pos) {
+	return (pos << 3) + 4;
 }
 
-inline int fromCanvas( int pos )
-{
+inline int fromCanvas(int pos) {
 	return roundDown(pos - 4, 8);
 }
 
-inline QPoint toCanvas(const QPoint *pos)
-{
-	return QPoint( toCanvas(pos->x()), toCanvas(pos->y()) );
+inline QPoint toCanvas(const QPoint *pos) {
+	return QPoint(toCanvas(pos->x()), toCanvas(pos->y()));
 }
 
-inline QPoint fromCanvas(const QPoint *pos)
-{
-	return QPoint( fromCanvas(pos->x()), fromCanvas(pos->y()) );
+inline QPoint fromCanvas(const QPoint *pos) {
+	return QPoint(fromCanvas(pos->x()), fromCanvas(pos->y()));
 }
 
-inline QPoint toCanvas( const QPoint & pos )
-{
-	return QPoint( toCanvas(pos.x()), toCanvas(pos.y()) );
+inline QPoint toCanvas(const QPoint & pos) {
+	return QPoint(toCanvas(pos.x()), toCanvas(pos.y()));
 }
 
-inline QPoint fromCanvas( const QPoint & pos )
-{
-	return QPoint( fromCanvas(pos.x()), fromCanvas(pos.y()) );
+inline QPoint fromCanvas(const QPoint & pos) {
+	return QPoint(fromCanvas(pos.x()), fromCanvas(pos.y()));
 }
 
-inline int roundDouble( double x )
-{
+inline int roundDouble(double x) {
 	return int(floor(x + 0.5));
 }
 
-inline double qpoint_distance( const QPoint & p1, const QPoint & p2 )
-{
+inline double qpoint_distance(const QPoint &p1, const QPoint &p2) {
 	return hypot(p1.x() - p2.x(), p1.y() - p2.y());
 }
 
-inline int snapToCanvas(int x)
-{
+inline int snapToCanvas(int x) {
 	return toCanvas(roundDown(x, 8));
 }
 
-inline int snapToCanvas( double x )
-{
+inline int snapToCanvas(double x) {
 	return snapToCanvas(int(x));
 }
 
