37b9431e61915d44cb77bd2237d8239c9b02c45e
Zoltan Padrah <zoltan_padrah@users.sf.net>
2010-01-21 00:17:25 +0100

    my first commit, yay

---
+++ test-1/mytest-1.cpp
@@ -0,0 +1,15 @@
+
+ class MyFirstTest: public QObject
+ {
+     Q_OBJECT
+ private slots:
+     void initTestCase()
+     { qDebug("called before everything else"); }
+     void myFirstTest()
+     { QVERIFY(1 == 1); }
+     void mySecondTest()
+     { QVERIFY(1 != 2); }
+     void cleanupTestCase()
+     { qDebug("called after myFirstTest and mySecondTest"); }
+ };
+ 
