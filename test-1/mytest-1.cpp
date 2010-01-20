23907e3d9ef8c8ac23c60a7a717bd6a46b49e1c3
Zoltan Padrah <zoltan_padrah@users.sf.net>
2010-01-21 00:37:41 +0100

    first testcase works

--- test-1/mytest-1.cpp
+++ test-1/mytest-1.cpp
@@ -1,15 +1,26 @@
+#include <QtTest/QtTest>
 
- class MyFirstTest: public QObject
+ class MyFirstTest : public QObject
  {
      Q_OBJECT
  private slots:
-     void initTestCase()
-     { qDebug("called before everything else"); }
-     void myFirstTest()
-     { QVERIFY(1 == 1); }
-     void mySecondTest()
-     { QVERIFY(1 != 2); }
-     void cleanupTestCase()
-     { qDebug("called after myFirstTest and mySecondTest"); }
+     void initTestCase() { 
+        qDebug("called before everything else"); 
+     }
+     
+     void myFirstTest()   { 
+        QVERIFY(1 == 1); 
+     }
+     
+     void mySecondTest() { 
+        QVERIFY(1 != 2); 
+     }
+     
+     void cleanupTestCase(){ 
+        qDebug("called after myFirstTest and mySecondTest"); 
+     }
  };
+ 
+  QTEST_MAIN(MyFirstTest)
+ #include "mytest-1.moc"
  
