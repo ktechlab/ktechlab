f00e3371db0b12cac89da9f0042cb4d262a4a92c
Julian Bäume <julian@svg4all.de>
2010-02-09 11:22:41 +0100

    add first version of abstract router-plugin
    This adds a base class for router-plugins. These plugins will provide
    routing for documents containing components and connectors between
    components. Since there is no efficient way of calculating optimal
    routes, yet, this brings the possibility to implement different
    heuristics of providing good routes.

---
+++ src/interfaces/iconrouter.cpp
@@ -0,0 +1,75 @@
+/*
+    Abstraction for the routing of a connection
+    Copyright (C) 2003-2004 by David Saxton <david@bluehaze.org>
+    Copyright (C) 2010  Julian Bäume <julian@svg4all.de>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+#include "iconrouter.h"
+
+#include <QPointF>
+#include <QPainterPath>
+#include <KDebug>
+
+using namespace KTechLab;
+
+IConRouter::IConRouter()
+    : m_documentModel(0)
+{}
+
+IConRouter::~IConRouter()
+{}
+
+QPainterPath IConRouter::paintedRoute() const
+{
+    QPainterPath p;
+    if (m_route.size() < 2)
+        return p;
+
+    QList<QPointF>::const_iterator end = m_route.constEnd();
+    QList<QPointF>::const_iterator it=m_route.constBegin();
+    p.moveTo(*it);
+    for ( ++it; it != end; ++it ){
+        p.lineTo(*it);
+    }
+    return p;
+}
+
+QList< QPointF > IConRouter::route() const
+{
+    return m_route;
+}
+
+void IConRouter::setRoute(const QList< QPointF >& route)
+{
+    if (m_route.size()>0) {
+        //this can happen, but in debug-mode, we should inform about it
+        kDebug() << "Overwriting non-empty route.";
+    }
+
+    m_route = route;
+}
+
+void IConRouter::setDocumentModel(IDocumentModel* model)
+{
+    if (!m_documentModel){
+        //this can happen, but in debug-mode, we should inform about it
+        kDebug() << "Overwriting documentModel";
+    }
+
+    m_documentModel = model;
+}
