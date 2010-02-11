8ea23a34941f0a812749cc030cc15db6230eebca
Julian Bäume <julian@svg4all.de>
2010-02-11 17:09:26 +0100

    update copyright notice

--- src/plugins/circuit/circuitview.cpp
+++ src/plugins/circuit/circuitview.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009 Julian Bäume <julian@svg4all.de>
+ * Copyright 2009-2010 Julian Bäume <julian@svg4all.de>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
