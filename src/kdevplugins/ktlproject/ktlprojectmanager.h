994a9f64d0ba7fe5098c1c6dbe3b0fa08d37fab6
Julian Bäume <julian@svg4all.de>
2010-03-30 14:34:43 +0200

    fix headers, it's 2010 now

--- src/kdevplugins/ktlproject/ktlprojectmanager.h
+++ src/kdevplugins/ktlproject/ktlprojectmanager.h
@@ -1,6 +1,6 @@
 /*
     Manage KTechLab project files.
-    Copyright (C) 2009  Julian Bäume <julian@svg4all.de>
+    Copyright (C) 2009-2010 Julian Bäume <julian@svg4all.de>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
