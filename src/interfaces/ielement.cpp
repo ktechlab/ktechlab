071e35c17e85985ca7b56f741568ddae499759cc
Zoltan Padrah <zoltan_padrah@users.sf.net>
2010-04-20 16:24:07 +0200

    add IElement interface
    this interface is exposed by all component models in ktechlab

---
+++ src/interfaces/ielement.cpp
@@ -0,0 +1,21 @@
+/***************************************************************************
+ *    IElement interface implementation                                    *
+ *       A commone interface for all compoent models in ktechlab           *
+ *    Copyright (c) 2010 Zoltan Padrah                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#include "ielement.h"
+
+using namespace KTechLab;
+
+/*
+IElement::IElement(){
+}
+*/
+IElement::~IElement(){
+}
