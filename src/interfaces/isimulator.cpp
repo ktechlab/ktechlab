4f443aa8c7d4878456edc96fffe5c419bdb96941
Zoltan Padrah <zoltan_padrah@users.sf.net>
2010-04-20 16:25:17 +0200

    add ISimulator interface
    this will be implemented by any simulator, for example CircuitSimulator

---
+++ src/interfaces/isimulator.cpp
@@ -0,0 +1,18 @@
+/***************************************************************************
+ *    ISimulator interface implementation                          *
+ *       A controller for simulations in KTechLab                          *
+ *     Copyright (c) 2010 Zoltan Padrah                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+ 
+#include "isimulator.h"
+#include "ielement.h"
+
+using namespace KTechLab;
+
+ISimulator::~ISimulator(){
+}
