5fcffcff268f16b068981ff0697ad31ba33a6267
Zoltan Padrah <zoltan_padrah@users.sf.net>
2010-07-27 19:03:53 +0300

    add an empty model for Capacitance

---
+++ src/plugins/basic_ec/elements/capacitance.h
@@ -0,0 +1,25 @@
+/***************************************************************************
+ *    Capacitance model                                                    *
+ *       used to simulate capacitances in KTechLab                         *
+ *    Copyright (c) 2010 Zoltan Padrah                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+
+#ifndef CAPACITANCE_H
+#define CAPACITANCE_H
+
+#include "interfaces/ielement.h"
+
+namespace KTechLab {
+
+class Capacitance : public IElement
+{
+};
+
+}
+#endif // CAPACITANCE_H
