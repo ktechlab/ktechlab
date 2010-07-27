dd5deb471f482970e8e1f42d240e14b1a3e378f3
Zoltan P <zoltan_padrah@users.sourceforge.net>
2010-07-27 20:00:56 +0300

    create initial mainpage file for doxygen and fill in a stub. also include the doc directory in the doxygen input path.
    building the doxygen documentation can be done by entering the
    following command in the main source directory:
    doxygen
    The result can be found in the build-doxyigen folder

---
+++ doc/devel/mainpage.h
@@ -0,0 +1,25 @@
+/**
+
+\mainpage
+
+\section Introduction
+
+This page in the documentation will describe the general architecture of KTechLab,
+from developer point of view.
+
+\section Terminology
+
+Element
+Component
+Simulator
+
+\section Adding support for new Elements (component models)
+
+\section Adding support for new routing algorithms
+
+\section Adding support for new document types
+
+TODO write this document
+
+*/
+
