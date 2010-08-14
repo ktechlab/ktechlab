7beba2599c01c03087a68a80d5f3b0d4695dd69d
Zoltan P <zoltan_padrah@users.sourceforge.net>
2010-08-15 01:13:12 +0300

    initial port of the resistance model.
    some methods are commented out, because the functionality needed for them is missing currently

--- src/plugins/basic_ec/elements/resistance.cpp
+++ src/plugins/basic_ec/elements/resistance.cpp
@@ -10,3 +10,80 @@
  ***************************************************************************/
 
 #include "resistance.h"
+
+#include "kdebug.h"
+
+using namespace KTechLab;
+
+Resistance::Resistance(const double resistance): IElement()
+{
+    if( resistance < 0 ){
+        kError() << "BUG: negative value for a resistance?!\n";
+    }
+    if( resistance < 1e-9 )
+        m_g = 1e9;
+    else
+        m_g = 1.0 / resistance;
+    // TODO set the number of pins?
+}
+
+Resistance::~Resistance()
+{
+
+}
+
+void Resistance::setConductance(const double g)
+{
+    #if 0
+    // FIXME what's going on, in the original?
+    if (g == m_g)
+        return;
+
+    if (p_eSet)
+        p_eSet->setCacheInvalidated();
+
+    m_g = g - m_g;
+    add_initial_dc();
+
+    m_g = g;
+    #endif
+}
+
+void Resistance::setResistance(const double r)
+{
+    if( r < 1e-9 )
+        setConductance(1e9);
+    else
+        setConductance(1. / r);
+}
+
+#if 0
+// methods from initial file
+void Resistance::add_initial_dc() {
+    if (!b_status) return;
+
+    A_g(0, 0) += m_g;
+    A_g(1, 1) += m_g;
+    A_g(0, 1) -= m_g;
+    A_g(1, 0) -= m_g;
+}
+
+void Resistance::updateCurrents() {
+    if (!b_status) return;
+
+    const double i = (p_cnode[0]->voltage() - p_cnode[1]->voltage()) * m_g;
+
+    p_cnode[0]->sinkCurrent(i);
+    p_cnode[1]->sourceCurrent(i);
+}
+#endif
+
+double Resistance::resistance() const
+{
+    return 1 / m_g;
+}
+
+double Resistance::conductance() const
+{
+    return m_g;
+}
--- src/plugins/basic_ec/elements/resistance.h
+++ src/plugins/basic_ec/elements/resistance.h
@@ -13,8 +13,45 @@
 
 namespace KTechLab {
 
+/**
+\brief Model for an ideal resistance
+
+*/
 class Resistance : public IElement
 {
+    public:
+        /**
+        constructor
+        \param resistance value of the resistance, in ohms
+            1 Ohm, by default
+        */
+        Resistance(const double resistance = 1);
+        /** destructor */
+        virtual ~Resistance();
+
+        /**
+        set the conductance of the resistance, in Siemens
+        \param g the new resistance, expressed by conductance, in Siemens
+        */
+        void setConductance(const double g);
+        
+        /**
+        set the resistance of the resistance, in Ohms
+        \param r new value of the resistance, in Ohms
+        */
+        void setResistance(const double r);
+
+        /**
+        \return the value of the resistance, in Ohms
+        */
+        double resistance() const ;
+        /**
+        \return the value of the conductance of the resistance, in Siemens
+        */
+        double conductance() const ;
+        
+    private:
+        double m_g; ///< conductance the resistance
 };
 
 }
