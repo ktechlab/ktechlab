4c60bcd1d293501aa58cacb56b29dc5d0cd7d627
Zoltan Padrah <zoltan_padrah@users.sf.net>
2010-08-15 16:58:33 +0300

    documentation on the circuit simulation and spelling fixes

--- doc/devel/mainpage.h
+++ doc/devel/mainpage.h
@@ -20,12 +20,16 @@ Various terms used in this document are described here.
 \li Simulator: class that simulates the behaviour of a circuit
 
 --
-\li CNode: component node, used in simulation, equivalend of pins?
+\li CNode: component node, used in simulation, equivalent of pins?
+
+\li Circuit Model: the storage place for the description of the structure and status of a circuut
+
+\li *Item: graphical classes, inherited from QGraphicsItem
 
 \section src_org Organization of the source code
 
 KTechLab internally is based on KDevPlatform.
-See http://www.kdevelop.org/mediawiki/index.php/KDevelop_4#Platform_Information
+See http://www.kdevelop.org/mediawiki/index.php/KDevelop_4#Platform_Information .
 The source code has similar layout as the above mentioned platform.
 It uses plugins.
 
@@ -56,6 +60,21 @@ TODO
 \subsection sim_manager SimulationManager
 
 TODO
+
+\subsection electr_simulation Workings of circuit simulation
+
+When an electronic circuit is simulated, the following processes take place:
+\li user interaction with the circuit: the model is changed in order to reflect the new situation;
+    event flow: user -> gui -> circuit scene -> model -> simulator update, based on the model
+\li visual representation redraw: the components are drawn periodically;
+    event flow: gui refresh timer ticks -> drawing performed, based only the state of the model;
+    approximate timer frequency: less than 50 - 60 Hz
+\li simulation time advance: time passes, so some signals (current, voltage) might change;
+    event flow: simulation timer ticks -> simulator calculates new voltage and current values ->
+        simulator stores the calculated values in the model
+
+The CircuitModel, subclass of IDocumentModel, currently consists of a set of components and connectors,
+both having the type of QVariantMap.
 
 \section new_elements Adding support for new Elements (component models)
 
