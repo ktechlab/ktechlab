f87432d27b6c4cc669b66c3c6dd4c11605656581
Zoltan P <zoltan_padrah@users.sourceforge.net>
2010-08-20 00:47:27 +0300

    create stubs for ElementSet and include it in the build process

---
+++ src/plugins/simulator/elementset.cpp
@@ -0,0 +1,89 @@
+/*
+    KTechLab, an IDE for electronics
+    Copyright (C) 2010 Zoltan Padrah
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+#include "elementset.h"
+
+#include "math/matrix.h"
+#include "math/quickmatrix.h"
+
+KTechLab::ElementSet::ElementSet()
+{
+    // TODO implement
+}
+
+KTechLab::ElementSet::~ElementSet()
+{
+    // TODO implement
+}
+
+void KTechLab::ElementSet::addElement(KTechLab::IElement* element)
+{
+    // TODO implement
+}
+
+void KTechLab::ElementSet::removeElement(KTechLab::IElement* element)
+{
+    // TODO implement
+}
+
+void KTechLab::ElementSet::solveEquations()
+{
+    // TODO implement
+}
+
+double& KTechLab::ElementSet::A_g(const unsigned int i, const unsigned int j)
+{
+    // TODO implement
+}
+
+double& KTechLab::ElementSet::A_b(const unsigned int i, const unsigned int j)
+{
+    // TODO implement
+}
+
+double& KTechLab::ElementSet::A_c(const unsigned int i, const unsigned int j)
+{
+    // TODO implement
+}
+
+double& KTechLab::ElementSet::A_d(const unsigned int i, const unsigned int j)
+{
+    // TODO implement
+}
+
+double& KTechLab::ElementSet::b_i(const unsigned int i)
+{
+    // TODO implement
+}
+
+double& KTechLab::ElementSet::b_v(const unsigned int i)
+{
+    // TODO implement
+}
+
+double KTechLab::ElementSet::x_j(const unsigned int i)
+{
+    // TODO implement
+}
+
+double KTechLab::ElementSet::x_v(const unsigned int i)
+{
+    // TODO implement
+}
