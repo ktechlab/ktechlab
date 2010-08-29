ae90bf5a1c7cdef060922b64770cab2d3ef5021b
Zoltan P <zoltan_padrah@users.sourceforge.net>
2010-08-29 23:03:31 +0300

    implement most methods of PinGroup

--- src/plugins/simulator/pingroup.cpp
+++ src/plugins/simulator/pingroup.cpp
@@ -19,38 +19,83 @@
 */
 
 #include "pingroup.h"
+#include <qlinkedlist.h>
+#include <qmap.h>
+#include <interfaces/simulator/iwire.h>
+#include <interfaces/simulator/ipin.h>
+#include <kdebug.h>
 
 using namespace KTechLab;
 
 KTechLab::PinGroup::PinGroup(QList< IPin* > pins, QList< IWire* > wires, IPin* startPin)
 {
- // TODO implement
+    // build auxiliaty maps
+    QMap<IPin*, IWire*> pinToWire;
+    foreach(IWire* wire, wires){
+        pinToWire.insertMulti(wire->startPin(), wire);
+        pinToWire.insertMulti(wire->endPin(), wire);
+    }
+
+    QMap<IPin*, IPin*> edges;
+    edges.clear();
+    foreach(IWire *wire, wires){
+        edges.insertMulti(wire->startPin(), wire->endPin());
+    }
+
+    m_pins.clear();
+    m_wires.clear();
+
+    // this is a breath-first traversing of a graph
+    m_pins.append(startPin);
+    QLinkedList<IPin*> toBeInspected;
+    toBeInspected.clear();
+    toBeInspected.append(startPin);
+    while( ! toBeInspected.isEmpty() ){
+        IPin * currentPin = toBeInspected.takeFirst();
+        QList<IPin*> currentAdiacent = edges.values(currentPin);
+        foreach(IPin * otherPin, currentAdiacent){
+            toBeInspected.append(otherPin);
+            m_pins.append(otherPin);
+        }
+        /* get the wires. because we want all the wires, if any end of a wire is
+        inside of the selected pins, the wire should be taken */
+        QList<IWire*> linkedWires = pinToWire.values(currentPin);
+        foreach(IWire *wire, linkedWires){
+            m_wires.append(wire);
+        }
+    }
+    // some debug
+    kDebug() << "pin group created with " << m_pins.size() << " pins and " <<
+                m_wires.size() << " wires \n";
 }
 
 KTechLab::PinGroup::~PinGroup()
 {
- // TODO implement
+    m_wires.clear();
+    m_pins.clear();
 }
 
 bool KTechLab::PinGroup::containsPin(IPin* pin) const
 {
- // TODO implement
+    return m_pins.contains(pin);
 }
 
 const QList< IPin* > PinGroup::pins() const
 {
- // TODO implement
+    return m_pins;
 }
 
 
 bool KTechLab::PinGroup::containsWire(IWire* wire) const
 {
- // TODO implement
+    return m_wires.contains(wire);
 }
 
 void KTechLab::PinGroup::setVoltage(double voltage)
 {
- // TODO implement
+    foreach(IPin *pin, m_pins){
+        pin->setVoltage(voltage);
+    }
 }
 
 void KTechLab::PinGroup::calculateInternalCurrents()
