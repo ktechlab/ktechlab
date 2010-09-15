72e222e0de42ede9417d2cbbe8e0b16a8dad2aa6
Zoltan Padrah <zoltan_padrah@users.sf.net>
2010-09-16 00:23:59 +0300

    add current signal

--- testing/simulatortest.cpp
+++ testing/simulatortest.cpp
@@ -8,6 +8,7 @@
 #include "capacitance.h"
 #include "cccs.h"
 #include "ccvs.h"
+#include "currentsignal.h"
 
 #include "qdebug.h"
 
@@ -25,11 +26,13 @@ void SimulatorTest::createTest(){
     Capacitance *c1 = new Capacitance(1e-6, 1e-9);
     CCCS *cccs1 = new CCCS( 2.0 );
     CCVS *ccvs1 = new CCVS( 1.5 );
+    CurrentSignal *i1 = new CurrentSignal(1e-6, 1e-3);
     
     circ->addElement(q1);
     circ->addElement(c1);
     circ->addElement(cccs1);
     circ->addElement(ccvs1);
+    circ->addElement(i1);
 
     sim->attachCircuit(circ);
 
