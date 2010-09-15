97707c91a83ef6ca793bd336051308322045ba8a
Zoltan Padrah <zoltan_padrah@users.sf.net>
2010-09-15 23:37:27 +0300

    add capacitance

--- testing/simulatortest.cpp
+++ testing/simulatortest.cpp
@@ -2,16 +2,21 @@
 #include "simulator.h"
 
 #include "bjt.h"
+#include "capacitance.h"
 
 int main(void){
     Simulator * sim = Simulator::self();
 
     sim->slotSetSimulating(false);
 
-    BJT *q1 = new BJT(true);
     Circuit *circ = new Circuit();
+
+    BJT *q1 = new BJT(true);
+    Capacitance *c1 = new Capacitance(1e-6, 1e-9);
     
     circ->addElement(q1);
+    circ->addElement(c1);
+
     sim->attachCircuit(circ);
 
     qDebug() << "simulator: " << sim << "\n";
