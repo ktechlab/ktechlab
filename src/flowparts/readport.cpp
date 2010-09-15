cfadae1a4dd3064e86e96365715e73491b3d33ed
Zoltan Padrah <zoltan_padrah@users.sf.net>
2010-09-15 18:18:51 +0300

    add flowcode, because it's needed

---
+++ src/flowparts/readport.cpp
@@ -0,0 +1,68 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by David Saxton                                    *
+ *   david@bluehaze.org                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#include "readport.h"
+
+#include "libraryitem.h"
+#include "flowcode.h"
+
+#include <klocale.h>
+
+Item* ReadPort::construct( ItemDocument *itemDocument, bool newItem, const char *id )
+{
+	return new ReadPort( (ICNDocument*)itemDocument, newItem, id );
+}
+
+LibraryItem* ReadPort::libraryItem()
+{
+	return new LibraryItem(
+		"flow/readport",
+		i18n("Read from Port"),
+		i18n("I\\/O"),
+		"portread.png",
+		LibraryItem::lit_flowpart,
+		ReadPort::construct );
+}
+
+ReadPort::ReadPort( ICNDocument *icnDocument, bool newItem, const char *id )
+	: FlowPart( icnDocument, newItem, id ? id : "readport" )
+{
+	m_name = i18n("Read from Port");
+	initIOSymbol();
+	createStdInput();
+	createStdOutput();
+	
+	createProperty( "0-port", Variant::Type::Port );
+	property("0-port")->setToolbarCaption( i18n("Read") );
+	property("0-port")->setEditorCaption( i18n("Port") );
+	property("0-port")->setValue("PORTA");
+	
+	createProperty( "1-var", Variant::Type::VarName );
+	property("1-var")->setToolbarCaption( i18n( "read from port to x", "to" ) );
+	property("1-var")->setEditorCaption( i18n("Variable") );
+	property("1-var")->setValue("x");
+}
+
+
+ReadPort::~ReadPort()
+{
+}
+
+void ReadPort::dataChanged()
+{
+	setCaption( i18n("Read %1 to %2").arg(dataString("0-port")).arg(dataString("1-var")) );
+}
+
+void ReadPort::generateMicrobe( FlowCode *code )
+{
+	code->addCode( dataString("1-var")+" = "+dataString("0-port") );
+	code->addCodeBranch( outputPart("stdoutput") );
+}
+
