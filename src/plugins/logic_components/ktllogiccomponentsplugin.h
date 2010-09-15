7422e6d5a3806ec23ceb5fac158fb65f90acb4c3
Julian Bäume <julian@svg4all.de>
2010-09-15 14:32:02 +0200

    add first draft of logic components plugin
    for now, it provides a few gates with only 2 inputs.

---
+++ src/plugins/logic_components/ktllogiccomponentsplugin.h
@@ -0,0 +1,41 @@
+/***************************************************************************
+ *   Copyright (C) 2009 Julian Bäume <julian@svg4all.de>                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#ifndef KTLLOGICPLUGIN_H
+#define KTLLOGICPLUGIN_H
+
+#include "interfaces/component/icomponentplugin.h"
+
+#include <QVariantList>
+
+namespace KTechLab
+{
+
+class KTLLogicComponentsFactory;
+class LogicElementFactory;
+
+class KTLLogicComponentsPlugin : public IComponentPlugin
+{
+    Q_OBJECT
+public:
+    KTLLogicComponentsPlugin( QObject *parent, const QVariantList& args );
+    virtual ~KTLLogicComponentsPlugin();
+    virtual void unload();
+
+private:
+    void init();
+
+    KTLLogicComponentsFactory * m_componentFactory;
+
+    LogicElementFactory * m_logicElementFactory;
+};
+
+}
+
+#endif
