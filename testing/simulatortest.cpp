db21610fb6f0a8860c52db0a12e2d0e77300b33c
Zoltan Padrah <zoltan_padrah@users.sf.net>
2010-09-15 23:27:48 +0300

    add BJT

--- testing/simulatortest.cpp
+++ testing/simulatortest.cpp
@@ -1,8 +1,19 @@
 #include "qdebug.h"
 #include "simulator.h"
 
+#include "bjt.h"
+
 int main(void){
-    Simulator * s = Simulator::self();
-    qDebug() << "simulator: " << s << "\n";
+    Simulator * sim = Simulator::self();
+
+    sim->slotSetSimulating(false);
+
+    BJT *q1 = new BJT(true);
+    Circuit *circ = new Circuit();
+    
+    circ->addElement(q1);
+    sim->attachCircuit(circ);
+
+    qDebug() << "simulator: " << sim << "\n";
 }
 
