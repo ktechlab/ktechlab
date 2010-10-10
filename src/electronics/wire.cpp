67a9988b2dee7773d1dbabb32554bfdc3bdcef45
Zoltan Padrah <zoltan_padrah@users.sf.net>
2010-10-10 13:24:45 +0200

    wire: convert tabs to 4 spaces

--- src/electronics/wire.cpp
+++ src/electronics/wire.cpp
@@ -18,36 +18,36 @@
 #include "simulator.h"
 
 Wire::Wire(Pin *startPin, Pin *endPin) :
-	m_current(NAN), m_bCurrentIsKnown(false)
+    m_current(NAN), m_bCurrentIsKnown(false)
 {
-	assert(startPin && endPin);
+    assert(startPin && endPin);
 
-	m_pStartPin = startPin;
-	m_pEndPin = endPin;
+    m_pStartPin = startPin;
+    m_pEndPin = endPin;
 
-	m_pStartPin->addWire(this);
-	m_pEndPin->addWire(this);
+    m_pStartPin->addWire(this);
+    m_pEndPin->addWire(this);
 }
 
 Wire::~Wire()
 {
-// if this fails, we've already been deleted. Because the memory allocation subsystem isn't fine-grained, we 
-// don't immediately segfault when the caller tries to call us, but rather further down when we try to de-refference 
-// the pins which we zeroed out below. This assertion should be left in place until the underlying bug is fixed. 
+// if this fails, we've already been deleted. Because the memory allocation subsystem isn't fine-grained, we
+// don't immediately segfault when the caller tries to call us, but rather further down when we try to de-refference
+// the pins which we zeroed out below. This assertion should be left in place until the underlying bug is fixed.
 assert(m_pStartPin && m_pEndPin);
 
-	m_pStartPin->removeWire(this);
-	m_pEndPin->removeWire(this);
+    m_pStartPin->removeWire(this);
+    m_pEndPin->removeWire(this);
 
-	// make sure we're conspicuously invalid to help debugging, cuz 
-	// there are many dangling refferences to this class. =(
-	m_pStartPin = m_pEndPin = 0;
+    // make sure we're conspicuously invalid to help debugging, cuz
+    // there are many dangling refferences to this class. =(
+    m_pStartPin = m_pEndPin = 0;
 }
 
 double Wire::currentFor(const Pin *aPin) const {
-	if(aPin == m_pStartPin) return m_current;
-	if(aPin == m_pEndPin ) return -m_current;
-	return 0;
+    if(aPin == m_pStartPin) return m_current;
+    if(aPin == m_pEndPin ) return -m_current;
+    return 0;
 }
 
 bool Wire::calculateCurrent()
@@ -75,19 +75,19 @@ bool Wire::calculateCurrent()
 
 double Wire::voltage() const
 {
-	double temp;
-	if((temp = m_pStartPin->voltage() - m_pEndPin->voltage()) && Simulator::self()->isSimulating()) {
-		// kdError() << k_funcinfo << "Wire voltage error: " << temp << endl;
-		qCritical() << "Wire voltage error: " << temp << endl;
-	}
+    double temp;
+    if((temp = m_pStartPin->voltage() - m_pEndPin->voltage()) && Simulator::self()->isSimulating()) {
+        // kdError() << k_funcinfo << "Wire voltage error: " << temp << endl;
+        qCritical() << "Wire voltage error: " << temp << endl;
+    }
 
-	return m_pStartPin->voltage();
+    return m_pStartPin->voltage();
 }
 
 void Wire::setCurrentKnown( bool known )
 {
-	m_bCurrentIsKnown = known;
-	if (!known) m_current = NAN;
+    m_bCurrentIsKnown = known;
+    if (!known) m_current = NAN;
 }
 
 /*!
