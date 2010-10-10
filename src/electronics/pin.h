22770134aabbfdc6918fe4be86829689ba330f1f
Zoltan Padrah <zoltan_padrah@users.sf.net>
2010-10-10 16:56:13 +0200

    Pin: update documentation comments

--- src/electronics/pin.h
+++ src/electronics/pin.h
@@ -1,6 +1,8 @@
 /***************************************************************************
  *   Copyright (C) 2005 by David Saxton                                    *
  *   david@bluehaze.org                                                    *
+ *   Modified by                                                           *
+ *    2010, Zoltan Padrah, zoltan_padrah@users.sourceforge.net             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -24,6 +26,13 @@ typedef std::set<Wire *> WireList;
 
 /**
 @author David Saxton
+
+@brief A pin of an element or a junction point
+
+A pin always has a voltage, and it can have a current flow from an element.
+It also can have one or more wires, through which current can flow,
+into other pins.
+
 */
 class Pin {
 
@@ -46,17 +55,21 @@ public:
 		gt_never = 20 // everything else
 	};
 
+    /** create a pin */
 	Pin();
+
+    /** Destructor */
 	~Pin();
 
 	/**
 	 * After calculating the nodal voltages in the circuit, this function should
 	 * be called to tell the pin what its voltage is.
+     * @param v the new voltage on the pin
 	 */
 	void setVoltage(double v);
 
 	/**
-	 * Returns the voltage as set by setVoltage.
+	 * @return the voltage as set by setVoltage.
 	 */
 	double voltage() const ;
 
@@ -100,7 +113,7 @@ public:
 	 */
 	int groundType() const { return m_groundType; }
 
-// ###  strange pin list stuff. 
+// ###  strange pin list stuff.
 	/**
 	 * Adds a dependent pin - one whose voltages will (or might) affect the
 	 * voltage of this pin. This is set by Component.
@@ -138,7 +151,8 @@ public:
 	 */
 	void setEqId(int id);
 	/**
-	 * The equation identifier.
+	 * The equation identifier. This identifier is a position on the
+     * circuit equations.
 	 * @see setEqId
 	 */
 	int eqId() const ;
@@ -148,35 +162,58 @@ public:
 	 * Returns a list of elements that will affect this pin (e.g. if this
 	 * pin is part of a resistor, then that list will contain a pointer to a
 	 * Resistance element)
+     * \TODO convert the list of elements to a simple pointer to an Element
 	 */
 	ElementList &elements() { return m_elementList; }
+
 	/**
 	 * Adds an element to the list of those that will affect this pin.
 	 */
 	void addElement(Element *e);
+
 	/**
 	 * Removes an element from the list of those that will affect this pin.
 	 */
 	void removeElement(Element *e);
+
 // #########################
 
+    /**
+     * Add a wire to the list of wires connected to this pin.
+     * @param wire the wire to be added
+     */
 	void addWire(Wire *wire);
+
+    /**
+     * Remove a wire from the list of wires connected to this pin.
+     */
 	void removeWire(Wire *wire);
 
+    /**
+     * @return the list of wires connected to the Pin.
+     */
     const WireList wires() const;
 
 private:
+        /// voltage of the Pin
 	double m_voltage;
+        /// current flowing from an element into the pin, if applicable
     double m_sourceCurrent;
 
+        /// is the current flowing from the Element known?
     bool m_currentIsKnown;
 
+        /// Equation ID
 	int m_eqId;
+
 	GroundType m_groundType;
 
 	PinSet m_circuitDependentPins;
-	PinSet m_groundDependentPins; // for circuit partitioning. 
+	PinSet m_groundDependentPins; // for circuit partitioning.
+
+        /// list of associated elements
 	ElementList m_elementList;
+        /// list of connected wires
 	WireList m_wireList;
 };
 
