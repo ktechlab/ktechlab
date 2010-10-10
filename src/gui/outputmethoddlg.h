6eaa5d75f23b7bd49328da8a8a252c5b823ecde3
Zoltan Padrah <zoltan_padrah@users.sf.net>
2010-10-10 20:26:06 +0200

    undo the removal of 4 files from SVN

---
+++ src/gui/outputmethoddlg.h
@@ -0,0 +1,57 @@
+/***************************************************************************
+ *   Copyright (C) 2003-2004 by David Saxton                               *
+ *   david@bluehaze.org                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#ifndef OUTPUTMETHODDLG_H
+#define OUTPUTMETHODDLG_H
+
+#include <kurl.h>
+
+class KTechlab;
+class MicroSelectWidget;
+class OutputMethodDlg;
+class OutputMethodWidget;
+
+class OutputMethodInfo
+{
+    public:
+        class Method
+        {
+            public:
+                enum Type
+                {
+                    Direct,
+                    SaveAndForget,
+                    SaveAndLoad
+                };
+        };
+
+        OutputMethodInfo();
+        // void initialize( OutputMethodDlg * dlg );
+
+        Method::Type method() const { return m_method; }
+        void setMethod( Method::Type method ) { m_method = method; }
+
+        bool addToProject() const { return m_bAddToProject; }
+        void setAddToProject( bool add ) { m_bAddToProject = add; }
+
+        QString picID() const { return m_picID; }
+        void setPicID( const QString & id ) { m_picID = id; }
+
+        KUrl outputFile() const { return m_outputFile; }
+        void setOutputFile( const KUrl & outputFile ) { m_outputFile = outputFile; }
+
+    protected:
+        Method::Type m_method;
+        bool m_bAddToProject;
+        QString m_picID;
+        KUrl m_outputFile;
+};
+
+#endif
