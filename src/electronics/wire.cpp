e950e0cd2acf5bb981b8d63ca4a7d90604732456
Zoltan P <zoltan_padrah@users.sourceforge.net>
2010-10-10 15:57:54 +0200

    wire: be clear the direction of the current
    here is supposed that all currents flow into the pin

--- src/electronics/wire.cpp
+++ src/electronics/wire.cpp
@@ -45,8 +45,14 @@ assert(m_pStartPin && m_pEndPin);
 }
 
 double Wire::currentFor(const Pin *aPin) const {
-    if(aPin == m_pStartPin) return m_current;
-    if(aPin == m_pEndPin ) return -m_current;
+    /*
+        Current flows from the start pin to the end pin.
+    */
+    if(aPin == m_pStartPin) return -m_current;
+    if(aPin == m_pEndPin ) return   m_current;
+
+    qWarning() << "BUG: requesting current for a pin not associated "
+        "with the connector";
     return 0;
 }
 
@@ -81,11 +87,13 @@ Pin *Wire::otherPin(const Pin *aPin) const
 
 void Wire::setCurrentFor(const Pin* pin, double current)
 {
+    /*  currents are flwoing into the pin;
+        in the wire, the current is from start to end */
     if(pin == m_pStartPin)
-        m_current = current;
+        m_current = -current;
     else
         if( pin == m_pEndPin)
-            m_current = -current;
+            m_current = current;
         else {
             qCritical() << "BUG: trying to set current on a non-associated end of a wire";
             return;
