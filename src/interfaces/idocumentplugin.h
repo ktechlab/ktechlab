451da41d52ee7ce1f7cbfce1dba480a0db228b88
Julian Bäume <julian@svg4all.de>
2010-10-12 18:26:57 +0200

    remove unneeded method
    this is provided by the component factories, now

--- src/interfaces/idocumentplugin.h
+++ src/interfaces/idocumentplugin.h
@@ -45,18 +45,6 @@ public:
     virtual ~IDocumentPlugin() {};
 
     /**
-     * Find the name of the file containing the graphical representation of
-     * the given component. The default implementation will simply normalize the
-     * components name to contain no slashes and add .svgz as the default suffix.
-     * You are encouraged to override this method in your own implementation to support
-     * multiple components per file.
-     *
-     * \param component - the name of the component
-     * \returns the filename containing the component
-     */
-    virtual QString fileNameForComponent( const QString &component );
-
-    /**
      * Each component factory can register itself at the document plugin, to make
      * it aware of it's presence. It will then determine which components the
      * factory has to offer and can handle them accordingly.
