3099317829cc963cab241b59f42e4e4d1899bbe8
Zoltan Padrah <zoltan_padrah@users.sf.net>
2010-11-04 00:44:20 +0100

    add properties to the Component class

--- src/electronics/component.cpp
+++ src/electronics/component.cpp
@@ -29,10 +29,52 @@ Component::Component()
 
 Component::~Component() {
 	removeElements();
+    // delete all properties
+    foreach(Property *p, m_propertyList){
+        if(p)
+            delete p;
+    }
+    m_propertyList.clear();
 }
 
 void Component::removeElements() {
 	m_elementMapList.clear();
+}
+
+void Component::addProperty(Property* theProperty)
+{
+    if(NULL == theProperty){
+        qCritical("Component::addProperty: trying to add NULL property!\n");
+    }
+    m_propertyList.append(theProperty);
+    connect( theProperty, SIGNAL(valueChanged(QVariant,QVariant)),
+             this, SLOT(propertyChanged(QVariant,QVariant)) );
+}
+
+void Component::propertyChanged(QVariant newValue, QVariant oldValue)
+{
+    Property *theSender = dynamic_cast<Property*>(sender());
+    if(!theSender){
+        qCritical() << "the sender of propertyChanged is null "
+            <<" or it's not a property!";
+        return;
+    }
+    propertyChanged(*theSender,newValue,oldValue);
+}
+
+
+QList< Property* > Component::properties() const
+{
+    return m_propertyList;
+}
+
+Property* Component::propertyByName(const QString& name) const
+{
+    foreach(Property *ret, m_propertyList){
+        if(ret->name() == name)
+            return ret;
+    }
+    return 0;
 }
 
 
