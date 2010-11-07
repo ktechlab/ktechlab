836aa46d5e0cde679c5ee2070284ee1c19c4f6c0
Zoltan Padrah <zoltan_padrah@users.sf.net>
2010-11-07 19:54:42 +0100

    make ec555 compile

--- src/electronics/components/ec555.cpp
+++ src/electronics/components/ec555.cpp
@@ -9,75 +9,17 @@
  ***************************************************************************/
 
 #include "ec555.h"
-#include "ecnode.h"
-#include "libraryitem.h"
 
-#include <kiconloader.h>
-#include <klocale.h>
-#include <qpainter.h>
+#include "pin.h"
 
-Item* EC555::construct(ItemDocument *itemDocument, bool newItem, const char *id)
+EC555::EC555()
+	: Component()
 {
-	return new EC555((ICNDocument*)itemDocument, newItem, id);
-}
-
-LibraryItem* EC555::libraryItem()
-{
-	return new LibraryItem(
-		"ec/555",
-		i18n("555"),
-		i18n("Integrated Circuits"),
-		"ic1.png",
-		LibraryItem::lit_component,
-		EC555::construct );
-}
-
-EC555::EC555(ICNDocument *icnDocument, bool newItem, const char *id)
-	: Component(icnDocument, newItem, (id) ? id : "555")
-{
-	m_name = i18n("555");
 
 	m_com1 = false;
 	m_com2 = false;
 	m_q    = false;
 
-	setSize(-32, -32, 64, 64);
-
-	// Pins down left
-	// Pin 7
-	discharge = &createPin(-40, -16, 0, "Dis")->pin();
-	addDisplayText("dis", QRect(-32, -24, 24, 16), "Dis");
-
-	// Pin 6
-	threshold = &createPin( -40, 0, 0, "Th" )->pin();
-	addDisplayText("th", QRect(-32, -8, 24, 16), "Th");
-
-	// Pin 2
-	trigger = &createPin(-40, 16, 0, "Trg")->pin();
-	addDisplayText("trg", QRect(-32, 8, 24, 16), "Trg");
-
-	// Top two
-	// Pin 8
-	vcc = &createPin(-16, -40, 90, "Vcc")->pin();
-	addDisplayText("vcc", QRect(-24, -32, 16, 8), "+");
-
-	// Pin 4
-	reset = &createPin(16, -40, 90, "Res")->pin();
-	addDisplayText("res", QRect(8, -28, 16, 16), "Res");
-
-	// Bottom two
-	// Pin 1
-	ground = &createPin(-16, 40, 270, "Gnd")->pin();
-	addDisplayText("gnd", QRect(-24, 20, 16, 8), "-");
-
-	// Pin 5
-	control = &createPin(16, 40, 270, "CV")->pin();
-	addDisplayText("cv", QRect(8, 12, 16, 16), "CV");
-
-	// Output on right
-	// Pin 3
-	output = &createPin(40, 0, 180, "Out")->pin();
-	addDisplayText("out", QRect(8, -8, 16, 16), "Out");
 
 	m_r1.setResistance(5e3);
 	m_r23.setResistance(1e4);
@@ -85,11 +27,6 @@ EC555::EC555(ICNDocument *icnDocument, bool newItem, const char *id)
 	m_po_source.setConductance(0);
 	m_r_discharge.setResistance(0.0001);
 
-	setup2pinElement(m_r1, *vcc, *control);
-	setup2pinElement(m_r23, *control, *ground);
-	setup2pinElement(m_po_sink, *output, *ground);
-	setup2pinElement(m_po_source, *output, *vcc);
-	setup2pinElement(m_r_discharge, *discharge, *ground);
 }
 
 EC555::~EC555() {}
