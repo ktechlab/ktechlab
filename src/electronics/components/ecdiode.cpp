7f0fd32413f51ffb4a89cf2072d5a4b8fd9b704c
Zoltan P <zoltan_padrah@users.sourceforge.net>
2010-11-14 20:36:49 +0100

    make ecdiode compile

--- src/electronics/components/ecdiode.cpp
+++ src/electronics/components/ecdiode.cpp
@@ -10,36 +10,40 @@
 
 #include "ecdiode.h"
 
+#include "variant.h"
 
+#include <QDebug>
 
 ECDiode::ECDiode()
 {
-    /*
+
 	DiodeSettings ds; // it will have the default properties that we use
 
-	createProperty("I_S", Variant::Type::Double);
-	property("I_S")->setCaption("Saturation Current");
-	property("I_S")->setUnit("A");
-	property("I_S")->setMinValue(1e-20);
-	property("I_S")->setMaxValue(1e-0);
-	property("I_S")->setValue(ds.I_S);
-	property("I_S")->setAdvanced(true);
+	Property * i_s =  new Property("I_S", Variant::Type::Double);
+	i_s->setCaption(tr("Saturation Current"));
+	i_s->setUnit("A");
+	i_s->setMinValue(1e-20);
+	i_s->setMaxValue(1e-0);
+	i_s->setValue(ds.I_S);
+	i_s->setAdvanced(true);
+    addProperty(i_s);
 
-	createProperty("N", Variant::Type::Double);
-	property("N")->setCaption(i18n("Emission Coefficient"));
-	property("N")->setMinValue(1.0);
-	property("N")->setMaxValue(1e1);
-	property("N")->setValue(ds.N);
-	property("N")->setAdvanced(true);
+    Property *n = new Property("N", Variant::Type::Double);
+	n->setCaption(tr("Emission Coefficient"));
+	n->setMinValue(1.0);
+	n->setMaxValue(1e1);
+	n->setValue(ds.N);
+	n->setAdvanced(true);
+    addProperty(n);
 
-	createProperty("V_B", Variant::Type::Double);
-	property("V_B")->setCaption(i18n("Breakdown Voltage"));
-	property("V_B")->setUnit("V");
-	property("V_B")->setMinAbsValue(1e-5);
-	property("V_B")->setMaxValue(1e10);
-	property("V_B")->setValue(ds.V_B);
-	property("V_B")->setAdvanced(true);
-    */
+    Property *v_b = new Property("V_B", Variant::Type::Double);
+	v_b->setCaption(tr("Breakdown Voltage"));
+	v_b->setUnit("V");
+	v_b->setMinAbsValue(1e-5);
+	v_b->setMaxValue(1e10);
+	v_b->setValue(ds.V_B);
+	v_b->setAdvanced(true);
+    addProperty(v_b);
 
 // 	createProperty( "R", Variant::Type::Double );
 // 	property("R")->setCaption( i18n("Series Resistance") );
@@ -52,39 +56,18 @@ ECDiode::ECDiode()
 
 ECDiode::~ECDiode() {}
 
-
-void ECDiode::setSaturationCurrent(double I_S)
+void ECDiode::propertyChanged(Property& theProperty, QVariant newValue, QVariant oldValue)
 {
+    Q_UNUSED(oldValue);
+
     DiodeSettings ds = m_diode.settings();
-    ds.I_S = I_S;
+    if(theProperty.name() == "I_S"){
+        ds.I_S = newValue.asDouble();
+    } else if(theProperty.name() == "N"){
+        ds.N = newValue.asDouble();
+    } else if(theProperty.name() == "V_B"){
+        ds.V_B = newValue.asDouble();
+    } else
+        qCritical() << "ECDiode: uknown property has changed: " << theProperty.name();
     m_diode.setDiodeSettings(ds);
-}
-
-void ECDiode::setEmissionCoefficient(double N)
-{
-    DiodeSettings ds = m_diode.settings();
-    ds.N = N;
-    m_diode.setDiodeSettings(ds);
-}
-
-void ECDiode::setBreakdownVoltage(double V_B)
-{
-    DiodeSettings ds = m_diode.settings();
-    ds.V_B = V_B;
-    m_diode.setDiodeSettings(ds);
-}
-
-double ECDiode::saturationCurrent() const
-{
-    return m_diode.settings().I_S;
-}
-
-double ECDiode::emissionCoefficient() const
-{
-    return m_diode.settings().N;
-}
-
-double ECDiode::breakdownVoltage() const
-{
-    return m_diode.settings().V_B;
 }
