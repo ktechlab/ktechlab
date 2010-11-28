f8442bd23bc1cfa150798be2836ba0aa639493de
Zoltan P <zoltan_padrah@users.sourceforge.net>
2010-11-28 14:53:33 +0100

    make Node an interface

--- src/node.h
+++ src/node.h
@@ -11,219 +11,59 @@
 #ifndef NODE_H
 #define NODE_H
 
+#include "connector.h"
+#include "typedefs.h"
+
+#include <QList>
+#include <QObject>
+
 #include <set>
 
-#include <canvas.h>
-//Added by qt3to4:
-#include <Q3ValueList>
-
-class CNItem;
-class ICNDocument;
-class ICNDocument;
-class Item;
-class Connector;
 class Node;
-class NodeData;
-class NodeGroup;
-class QTimer;
 
-typedef std::set<Connector *> ConnectorList;
-typedef Q3ValueList<Node *> NodeList;
 
 /**
-@short A standard node that can be associated with a Connector or a CNItem
-@author David Saxton
+@short A standard node that can be associated with a Connector
+@author David Saxton, Zoltan Padrah
+
+A node is associated to one or more pins in the simulation.
+Two nodes are connected by a \ref Connector.
+
+Note that a node might change the number of Pins it contains,
+during runtime.
 */
-class Node : public Q3CanvasPolygon {
+class Node : public QObject {
 	Q_OBJECT
 
 public:
-	// this shall disappear one day
-	/**
-	 * Used for run-time identification of the node:
-	 * Can be electronic node (so has values of current, voltage, etc)
-	 * or a pic part node
-	 * this enum will be cleared soon
-	 */
-	enum node_type {
-		ec_pin,
-		ec_junction,
-		fp_in,
-		fp_out,
-		fp_junction};
-
-	/**
-	 * @param dir the direction of the node; 0 degrees for left, 90 degrees for
-	 * up, etc in an anti-clockwise direction. An "up" node has the
-	 * wire-connection point at the top and the (component/flowpart)-end at the
-	 * bottom.
-	 */
-	Node(ICNDocument *icnDocument, Node::node_type type, int dir, const QPoint &pos, QString *id = 0);
+    /**
+     Create a Node with 1 associated pin and 0 connector.
+     */
+	Node();
 	virtual ~Node();
 
-	/**
-	 * Sets the node's visibility, as well as updating the visibility of the
-	 * attached connectors as appropriate
-	 */
-	virtual void setVisible(const bool yes);
-	/**
-	 * Returns the global id, that is unique to the node
-	 * amongst all the nodes on the canvas
-	 */
-	const QString id() const { return m_id; }
 
-	/**
-	 * Returns the id that is internal to the CNItem to which the
-	 * node belongs to. Returns a null QString if no parentitem
-	 */
-	const QString childId() const { return m_childId; }
+    /**
+     * Registers an input connector (i.e. this is the end node) as connected
+     * to this node.
+     */
+    virtual void addConnector(Connector *connector) = 0;
 
-	/**
-	 * Use this function to set the child-id, that is unique to the node
-	 * amongst the other nodes associated with its parent CNItem
-	 */
-	void setChildId(const QString &id) { m_childId = id; }
+    /**
+     * @return the list of all the connectors attached to the node
+     */
+    virtual ConnectorList allConnectors() const = 0;
 
-	/**
-	 * Sets the "level" of the node. By default, the level is 0. The level of
-	 * the node tells the node what CNItems it can be connected to through
-	 * a connector.
-	 * @see level
-	 */
-	virtual void setLevel(const int level);
-	/**
-	 * Returns the level of the nodes
-	 * @see setLevel
-	 */
-	int level() const { return m_level; }
+    /**
+     * @return the number of connectors connected to this node
+     */
+    virtual int numCon() const = 0;
 
-	/**
-	 * Sets the orientation of the node.
-	 */
-	void setOrientation(const int dir);
-	/**
-	 * Changes the lenght of the node. By default, this is 8. Some node types
-	 * (such as junctions) do not make use of this value.
-	 */
-	void setLength(const int length);
-	/**
-	 * Associates a CNItem with the node - ie the node belongs to the CNItem,
-	 * and hence gets deleted when the CNItem gets deleted.s
-	 */
-	virtual void setParentItem(CNItem *parentItem);
-	/**
-	 * Returns true if the node is part of a CNItem
-	 * (i.e. not between multiple connectors)
-	 */
-	bool isChildNode() const { return (p_parentItem != 0); }
+    /**
+     * Removes a specific connector
+     */
+    virtual void removeConnector(Connector *connector) = 0;
 
-	/**
-	 * Returns a pointer to the CNItem to which the node belongs,
-	 * or Null if it doesn't.
-	 */
-	CNItem *parentItem() const { return p_parentItem; }
-
-	NodeData nodeData() const;
-
-	void setNodeGroup(NodeGroup *ng) { p_nodeGroup = ng; }
-
-	NodeGroup *nodeGroup() const { return p_nodeGroup; }
-
-	/* interface common to ecnode and fpnode; these might be required by ItemDocumentData, ICNDocument  */
-	virtual bool isConnected(Node *node, NodeList *checkedNodes = 0) = 0;
-
-	virtual void removeConnector(Connector *connector) = 0;
-
-	/**
-	 * Returns the total number of connections to the node. This is the number
-	 * of connectors and the parent
-	 * item connector if it exists and is requested.
-	 * @param includeParentItem Count the parent item as a connector if it exists
-	 * @param includeHiddenConnectors hidden connectors are those as e.g. part of a subcircuit
-	 */
-	virtual int numCon(bool includeParentItem, bool includeHiddenConnectors) const = 0;
-
-	/**
-	 * @return the list of all the connectors attached to the node
-	 */
-	virtual ConnectorList getAllConnectors() const = 0;
-
-	/**
-	 * For a flownode: returns the first input connector, if it exist, or the fist outptut connector, if it exists.
-	 * For an electric node: returns the first connector
-	 * If the node isn't connected to anyithing, returns null ( 0 )
-	 * @return pointer to the desired connector
-	 */
-	virtual Connector* getAConnector() const = 0;
-
-	/**
-	 * Draw shape. Note that this has to remain public.
-	 */
-	virtual void drawShape(QPainter &p) = 0;
-
-public slots:
-        /*
-         * These slots have to match the exact signature of the signals connected it,
-         * or else the Qt signal/slot mechanism won't work (and get lots of regressions)
-         */
-	void moveBy(double dx, double dy);
-	
-	void removeNode(Item*) {
-// parameter is made available for subclasses. 
-		removeNode(); }
-
-	void removeNode();
-	void setNodeSelected(bool yes);
-
-signals:
-	void moved(Node *node);
-	/**
-	 * Emitted when the CNItem is removed. Normally, this signal is caught by associated
-	 * nodes, who will remove themselves as well.
-	 */
-	void removed(Node* node);
-
-protected:
-	virtual void initPoints();
-	/**
-	 * Moves and rotates (according to m_dir) the painter, so that our current
-	 * position is (0,0).
-	 */
-	void initPainter(QPainter &p);
-	/**
-	 * Undoes the effects of initPainter.
-	 */
-	void deinitPainter(QPainter &p);
-
-	/** If this node has precisely two connectors emerging from it, then this
-	 * function will trace the two connectors until the point where they
-	 * diverge; this point is returned. */
-	virtual QPoint findConnectorDivergePoint(bool *found) = 0;
-
-	/** The node's type. This member will be removed! */
-	node_type m_type;
-
-	int m_dir;
-	int m_length;
-
-// TODO: refactor to fpnode??
-	int m_level;
-// ### 
-
-	ICNDocument *p_icnDocument;
-	CNItem *p_parentItem;
-
-	NodeGroup *p_nodeGroup;
-
-// TODO: do we want this class to know about being selected/deselected? 
-// static members are weird.. Also, this is not configurable, as written. 
-	static QColor m_selectedColor;
-
-	bool b_deleted;
-private:
-// these fields are critical to saved circuit documents.
-	QString m_id;
-	QString m_childId;
 };
 
 #endif
