f77b24c80edc3f69548552906b15fca75f4ed864
Zoltan Padrah <zoltan_padrah@users.sf.net>
2010-11-28 14:56:17 +0100

    rewrite ElectronicConnector

--- src/electronics/electronicconnector.cpp
+++ src/electronics/electronicconnector.cpp
@@ -8,118 +8,73 @@
 // Copyright: See COPYING file that comes with this distribution
 //
 
-#include <cassert>
-#include <cmath>
-
-#include "circuiticndocument.h"
 #include "electronicconnector.h"
-#include "junctionnode.h"
-#include "ecnode.h"
 #include "wire.h"
+#include "ecnode.h"
 
-ElectronicConnector::ElectronicConnector(ECNode *startNode, ECNode *endNode,
-        ICNDocument *_ICNDocument, const QString &id)
-        : Connector(_ICNDocument, id), m_currentAnimationOffset(0) {
+#include "QDebug"
+
+ElectronicConnector::ElectronicConnector(ECNode *startNode, ECNode *endNode)
+        : Connector() {
+    Q_ASSERT(startNode);
+    Q_ASSERT(endNode);
+
+    int wirecount = startNode->numPins();
+
+    // same size?
+    if( startNode->numPins() != endNode->numPins() ){
+        qCritical() << "BUG: connecting 2 nodes with different pin count!"
+            << "Going for minimum!\n";
+        if(startNode->numPins() < endNode->numPins())
+            wirecount = startNode->numPins();
+        else
+            wirecount = endNode->numPins();
+    }
+
+    // nodes
     m_startEcNode = startNode;
     m_endEcNode = endNode;
 
-    p_icnDocument->registerItem(this);
-    p_icnDocument->requestRerouteInvalidatedConnectors();
-
-    if (startNode && endNode) {
-        connect(startNode, SIGNAL(numPinsChanged(unsigned)), this, SLOT(syncWiresWithNodes()));
-        connect(endNode, SIGNAL(numPinsChanged(unsigned)), this, SLOT(syncWiresWithNodes()));
-        syncWiresWithNodes();
+    // create wires
+    m_wires.clear();
+    for(int i=0; i<wirecount; i++){
+        Wire *w = new Wire( startNode->pin(i), endNode->pin(i));
+        m_wires.append(w);
     }
 }
 
 ElectronicConnector::~ElectronicConnector() {
-// Our nodes should have been notified that we've been deleted. 
-	assert(b_deleted);
-
-/* 
-I think what's going on here is that even though we create wires, they're *always* put under the control of Pins. 
-Therefore, it is the pins that are responsible for deleting them. As long as the assertion above always passes, then we
-know we've done our job and told both of our ECNodes that we're gone and the pins have been told to remove our wires. 
-
-Whether things actually work that way is another matter.
-*/
-
-    /* Even though we segfault here, we MUST be able to delete wires from this class!
-    Therefore we need to make sure our list of wires is accurate before we go into self destruct.
- 
-    unsigned size = m_wires.size();
-
-    for (unsigned i = 0; i < size; i++)
-        delete m_wires[i];   */
+    // wires
+    foreach(Wire *w, m_wires){
+        delete w;
+    }
+    m_wires.clear();
+    m_startEcNode = 0;
+    m_endEcNode = 0;
 }
 
-void ElectronicConnector::syncWiresWithNodes() {
-
-    ECNode *startEcNode = m_startEcNode;
-    ECNode *endEcNode = m_endEcNode;
-
-    if (!startEcNode || !endEcNode) return;
-
-    // FIXME more dynamic_cast to avoid using type() member
-    const bool isStartNodeJunction = dynamic_cast<JunctionNode*>(startEcNode) != 0;
-    const bool isEndNodeJunction   = dynamic_cast<JunctionNode*>(endEcNode)   != 0;
-    unsigned newNumWires = 0;
-
-    if (isStartNodeJunction || isEndNodeJunction)
-        newNumWires = QMAX(startEcNode->numPins(), endEcNode->numPins());
-    else newNumWires = QMIN(startEcNode->numPins(), endEcNode->numPins());
-
-    unsigned oldNumWires = m_wires.size();
-
-    if (newNumWires == oldNumWires) return;
-
-// Critical section? ################3
-    if (isStartNodeJunction)
-        startEcNode->setNumPins(newNumWires);
-
-    if (isEndNodeJunction)
-        endEcNode->setNumPins(newNumWires);
-
-// ####################################
-
-    if (newNumWires > oldNumWires) {
-        m_wires.resize(newNumWires);
-// FIXME: check this code against new scheme for storing pins directly in a vector. 
-        for (unsigned i = oldNumWires; i < newNumWires; i++) {
-            if (&startEcNode->pin(i) && &endEcNode->pin(i))
-                m_wires[i] = new Wire(&startEcNode->pin(i), &endEcNode->pin(i));
-        }
-    } else {
-        for (unsigned i = newNumWires; i < oldNumWires; i++)
-            delete m_wires[i];
-
-        m_wires.resize(newNumWires);
-    }
-
-    updateConnectorLines();
-
-// emit numWiresChanged(newNumWires);
+Node* ElectronicConnector::startNode() const
+{
+    return m_startEcNode;
 }
 
-void ElectronicConnector::incrementCurrentAnimation(double deltaTime) {
-    // The values and equations used in this function have just been developed
-    // empircally to be able to show a nice range of currents while still giving
-    // a good indication of the amount of current flowing
+Node* ElectronicConnector::endNode() const
+{
+    return m_endEcNode;
+}
 
-    double I_min = 1e-4;
-    double sf    = 3.0; // scaling factor
+int ElectronicConnector::numWires() const
+{
+    return m_wires.size();
+}
 
-    for (unsigned i = 0; i < m_wires.size(); ++i) {
-        assert(m_wires[i]);
-
-        double I = m_wires[i]->current();
-        double sign  = (I > 0) ? 1 : -1;
-        double I_abs = I * sign;
-        double prop  = (I_abs > I_min) ? std::log(I_abs / I_min) : 0.0;
-
-        m_currentAnimationOffset += deltaTime * sf * std::pow(prop, 1.3) * sign;
+Wire* ElectronicConnector::wire(unsigned int num) const
+{
+    if(num >= m_wires.size()){
+        qCritical() << "BUG: trying to access inexistent wire in a connector!\n";
+        return NULL;
     }
+    return m_wires.at(num);
 }
 
 #include "electronicconnector.moc"
