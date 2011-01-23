446d4acdb2390a417b5c5deafb663d2d8c0f4079
Zoltan P <zoltan_padrah@users.sourceforge.net>
2011-01-23 20:38:53 +0100

    update QprocesWithArguments

--- src/languages/qprocesswitharguments.cpp
+++ src/languages/qprocesswitharguments.cpp
@@ -1,21 +1,35 @@
-/*
-*/
+/***************************************************************************
+ *   Copyright (C) 2011 by Zoltan Padrah                                   *
+ *   zoltan_padrah@users.sourceforge.net                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
 #include <qprocesswitharguments.h>
 
 #include "QDebug"
 
-QProcessWithArguments::QProcessWithArguments(QObject* parent): QProcess(parent)
+QProcessWithArguments::QProcessWithArguments(QObject* parent):
+    QProcess(parent), m_app("")
 {
 
 }
 
 void QProcessWithArguments::start()
 {
-    qCritical() << "not implemented"; // FIXME
+    qDebug() << "starting executable: " << m_app << " with arguments: "
+        << m_args;
+    QProcess::start(m_app, m_args);
 }
 
 QProcessWithArguments& QProcessWithArguments::operator<<(QString args)
 {
-    qCritical() << "not implemented"; // FIXME
+    if(m_app == "")
+        m_app = args;
+    else
+        m_args.append(args);
     return *this;
 }
--- src/languages/qprocesswitharguments.h
+++ src/languages/qprocesswitharguments.h
@@ -1,17 +1,33 @@
-/*
-*/
+/***************************************************************************
+ *   Copyright (C) 2011 by Zoltan Padrah                                   *
+ *   zoltan_padrah@users.sourceforge.net                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+
 #ifndef _QPROCESSWITHARGUMENTS_H_
 #define _QPROCESSWITHARGUMENTS_H_
 
 #include <QObject>
 #include <qprocess.h>
 
+/**
+    A process class that allows building its argument list with
+    the help of << operator.
+*/
 class QProcessWithArguments : public QProcess
 {
     public:
         QProcessWithArguments(QObject* parent = 0);
         void start();
         QProcessWithArguments & operator<<(QString args);
+    private:
+        QString m_app;
+        QStringList m_args;
 };
 
 
