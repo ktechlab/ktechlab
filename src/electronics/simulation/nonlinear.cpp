0df11c6ca1cee3e4eaab75e2a9c404d715a62575
Zoltan P <zoltan_padrah@users.sourceforge.net>
2011-05-07 23:19:49 +0200

    revert Diode and Nonlinear code to the one at v0.3.7
    This way the diode seems to work correcly

--- src/electronics/simulation/nonlinear.cpp
+++ src/electronics/simulation/nonlinear.cpp
@@ -1,138 +1,190 @@
 /**************************************************************************
- *   Copyright (C) 2003-2005 by David Saxton                               *
- *   david@bluehaze.org                                                    *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- ***************************************************************************/
+*   Copyright (C) 2003-2005 by David Saxton                               *
+*   david@bluehaze.org                                                    *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+***************************************************************************/
 
+#include "matrix.h"
 #include "nonlinear.h"
 
 #include <cmath>
 using namespace std;
 
+const double KTL_MAX_DOUBLE = 1.7976931348623157e+308;///< 7fefffff ffffffff
+const int KTL_MAX_EXPONENT = int( log( KTL_MAX_DOUBLE ) );
+
+
 NonLinear::NonLinear()
-	: Element()
+: Element()
 {
 }
 
-// the simulator will break if you fail to clamp your voltage with this function. 
-double NonLinear::diodeVoltage(double V, double V_prev, double N, double V_lim) const
-{
-	double Vt = V_T * N;
 
-	if(V > V_lim && fabs(V - V_prev) > 2 * Vt) {
-		if(V_prev > 0) {
-			double arg = (V - V_prev) / Vt;
-			if(arg > 0) return V_prev + Vt * (2 + log(arg - 2));
-			else	    return V_prev - Vt * (2 + log(2 - arg));
-		} else		    return (V_prev < 0) ? (Vt * log (V / Vt)) : V_lim;
-	} else {
-		if(V < 0) {
-			double arg = (V_prev > 0) ? (-1 - V_prev) : (2 * V_prev - 1);
-			if (V < arg)
-				return arg;
-		}
-	}
-	return V;
+#ifndef MIN
+# define MIN(x,y) (((x) < (y)) ? (x) : (y))
+#endif
+
+#ifndef MAX
+# define MAX(x,y) (((x) > (y)) ? (x) : (y))
+#endif
+
+
+double NonLinear::diodeCurrent( double v, double I_S, double N ) const
+{
+    return I_S * (exp( MIN( v / (N * V_T), KTL_MAX_EXPONENT ) ) - 1);
 }
 
-double NonLinear::fetVoltageDS(double V, double V_prev) const
-{
-	if(V_prev >= 3.5) {
-		if(V > V_prev) return std::min(V, 3 * V_prev + 2);
-		else if(V < 3.5) return std::max(V, 2.5); // I think this is the pinch-off voltage. 
 
-		return V;
-	}
-	
-	if(V > V_prev) return std::min(V, 4.0);
-	
-	return std::max(V, -0.5);
+double NonLinear::diodeConductance( double v, double I_S, double N ) const
+{
+    double Vt = V_T * N;
+    return I_S * exp( MIN( v / Vt, KTL_MAX_EXPONENT ) ) / Vt;
 }
 
-// obtain the diode limit voltage.
-double NonLinear::diodeLimitedVoltage(double I_S, double N) const
+
+double NonLinear::diodeVoltage( double V, double V_prev, double N, double V_lim ) const
 {
-	double Vt = V_T * N;
-	return Vt * log(Vt / M_SQRT2 / I_S);
+    double Vt = V_T * N;
+    
+    if ( V > V_lim && fabs( V - V_prev ) > 2 * Vt ) {
+        if ( V_prev > 0 ) {
+            double arg = (V - V_prev) / Vt;
+            if (arg > 0)
+                V = V_prev + Vt * (2 + log( arg - 2 ));
+            else    V = V_prev - Vt * (2 + log( 2 - arg ));
+        } else      V = (V_prev < 0) ? (Vt * log (V / Vt)) : V_lim;
+    } else {
+        if ( V < 0 ) {
+            double arg = (V_prev > 0) ? (-1 - V_prev) : (2 * V_prev - 1);
+            if (V < arg)
+                V = arg;
+        }
+    }
+    return V;
 }
 
-/// combined function for current and conductance. 
-void NonLinear::diodeJunction(double V, double I_S, double N, double *I, double *g) const
-{
-	double Vt = V_T * N;	
 
-	if (V < -3 * Vt) {
-		double a = 3 * Vt / (V * M_E);
-		a  = a * a * a;
-		*I = -I_S * (1 + a);
-		*g =  I_S * 3 * a / V;
-	} else {
-		double e = exp(V * ELEMENTARY_CHARGE / (N * E_T));
-		*I = I_S * (e - 1);
-		*g = I_S * ELEMENTARY_CHARGE / (N * E_T) * e;
-	}
+double NonLinear::diodeLimitedVoltage( double I_S, double N ) const
+{
+    double Vt = N * V_T;
+    
+    return Vt * log( Vt / M_SQRT2 / I_S );
 }
 
-double NonLinear::fetVoltage(double V, double V_prev, double Vth) const
+
+void NonLinear::diodeJunction( double V, double I_S, double N, double * I, double * g ) const
 {
-	double V_tst_hi = 2 * fabs(V_prev - Vth) + 2.0;
-	double V_tst_lo = V_tst_hi / 2;
-	double V_tox = Vth + 3.5;
-	double delta_V = V - V_prev;
-
-	if(V_prev >= Vth) {
-		// on
-		if(V_prev >= V_tox) {
-			if(delta_V <= 0) {
-				// going off
-				if(V >= V_tox) {
-					if(-delta_V > V_tst_lo)
-						return V_prev - V_tst_lo;
-
-					return V;
-				}
-
-				return std::max(V, Vth + 2);
-			}
-
-			// staying on
-			if(delta_V >= V_tst_hi)
-				return V_prev + V_tst_hi;
-
-			return V;
-		}
-
-		// middle region
-		if(delta_V <= 0) {
-			// decreasing
-			return std::max(V, Vth - 0.5);
-		}
-
-		// increasing
-		return std::min(V, Vth + 4);
-	}
-
-	//  off
-	if(delta_V <= 0) {
-		// staying off
-		if(-delta_V > V_tst_hi)
-			return V_prev - V_tst_hi;
-
-		return V;
-	}
-
-	// going on
-	if(V <= Vth + 0.5) {
-		if(delta_V > V_tst_lo)
-			return V_prev + V_tst_lo;
-
-		return V;
-	}
-
-	return Vth + 0.5;
+    double Vt = N * V_T;
+    
+    if (V < -3 * Vt) {
+        double a = 3 * Vt / (V * M_E);
+        a = a * a * a;
+        *I = -I_S * (1 + a);
+        *g = +I_S * 3 * a / V;
+    } else {
+        double e = exp( MIN( V / Vt, KTL_MAX_EXPONENT ) );
+        *I = I_S * (e - 1);
+        *g = I_S * e / Vt;
+    }
 }
 
+
+double NonLinear::fetVoltage( double V, double V_prev, double Vth ) const
+{
+    double V_tst_hi = fabs( 2 * (V_prev - Vth) ) + 2.0;
+    double V_tst_lo = V_tst_hi / 2;
+    double V_tox = Vth + 3.5;
+    double delta_V = V - V_prev;
+    
+    if ( V_prev >= Vth ) {
+        // on
+        if ( V_prev >= V_tox ) {
+            if ( delta_V <= 0 ) {
+                // going off
+                if ( V >= V_tox ) {
+                    if ( -delta_V > V_tst_lo )
+                        return V_prev - V_tst_lo;
+                    
+                    return V;
+                }
+                
+                return MAX( V, Vth + 2 );
+            }
+            
+            // staying on
+            if ( delta_V >= V_tst_hi )
+                return V_prev + V_tst_hi;
+            
+            return V;
+        }
+        
+        // middle region
+        if ( delta_V <= 0 ) {
+            // decreasing
+            return MAX( V, Vth - 0.5 );
+        }
+        
+        // increasing
+        return MIN( V, Vth + 4 );
+    }
+    
+    //  off
+    if ( delta_V <= 0 ) {
+        // staying off
+        if ( -delta_V > V_tst_hi )
+            return V_prev - V_tst_hi;
+        
+        return V;
+    }
+    
+    // going on
+    if ( V <= Vth + 0.5 ) {
+        if ( delta_V > V_tst_lo )
+            return V_prev + V_tst_lo;
+        
+        return V;
+    }
+    
+    return Vth + 0.5;
+}
+
+
+double NonLinear::fetVoltageDS( double V, double V_prev ) const
+{
+    if ( V_prev >= 3.5 ) {
+        if ( V > V_prev )
+            return MIN( V, 3 * V_prev + 2 );
+        else if (V < 3.5)
+            return MAX( V, 2 );
+        
+        return V;
+    }
+    
+    if ( V > V_prev )
+        return MIN( V, 4 );
+    
+    return MAX( V, -0.5 );
+}
+
+
+void NonLinear::mosDiodeJunction( double V, double I_S, double N, double * I, double * g ) const
+{
+    double Vt = N * V_T;
+    
+    if ( V <= 0 )
+    {
+        *g = I_S / Vt;
+        *I = *g * V;
+    } else {
+        double e = exp( MIN ( V / Vt, KTL_MAX_EXPONENT ) );
+        *I = I_S * (e - 1);
+        *g = I_S * e / Vt;
+    }
+    
+    *I += V * I_S;
+    *g += I_S;
+}
--- src/electronics/simulation/nonlinear.h
+++ src/electronics/simulation/nonlinear.h
@@ -1,12 +1,12 @@
 /***************************************************************************
- *   Copyright (C) 2003-2005 by David Saxton                               *
- *   david@bluehaze.org                                                    *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- ***************************************************************************/
+*   Copyright (C) 2003-2005 by David Saxton                               *
+*   david@bluehaze.org                                                    *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+***************************************************************************/
 
 #ifndef NONLINEAR_H
 #define NONLINEAR_H
@@ -19,41 +19,50 @@
 */
 class NonLinear : public Element
 {
-public:
-	NonLinear();
-
-	virtual bool isNonLinear() const { return true; }
-
-	/**
-	* Newton-Raphson iteration: Update equation system.
-	*/
-	virtual void update_dc() = 0;
-
-protected:
-	/**
-	* Limits the diode voltage to prevent divergence in the nonlinear
-	* iterations.
-	*/
-	double diodeVoltage(double v, double V_prev, double N, double V_lim) const;
-
-	/**
-	* Current and conductance for a diode junction.
-	*/
-	void diodeJunction(double v, double I_S, double N, double *I, double *g) const;
-
-	/**
-	* Limits the drain-source voltage to prevent divergence in the
-	* nonlinear iterations.
-	*/
-	double fetVoltageDS(double V, double V_prev) const;
-
-	/**
-	* Limits the forward voltage to prevent divergence in the nonlinear
-	* iterations.
-	*/
-	double fetVoltage(double V, double V_prev, double Vth) const;
-
-	double diodeLimitedVoltage(double I_S, double N) const;
+    public:
+        NonLinear();
+        
+        virtual bool isNonLinear() const { return true; }
+        /**
+        * Newton-Raphson iteration: Update equation system.
+        */
+        virtual void update_dc() = 0;
+        
+    protected:
+        /**
+        * The diode current.
+        */
+        double diodeCurrent( double v, double I_S, double N ) const;
+        /**
+        * Conductance of the diode - the derivative of Schockley's
+        * approximation.
+        */
+        double diodeConductance( double v, double I_S, double N ) const;
+        /**
+        * Limits the diode voltage to prevent divergence in the nonlinear
+        * iterations.
+        */
+        double diodeVoltage( double v, double V_prev, double N, double V_lim ) const;
+        /**
+        * Current and conductance for a diode junction.
+        */
+        void diodeJunction( double v, double I_S, double N, double * I, double * g ) const;
+        /**
+        * Current and conductance for a MOS diode junction.
+        */
+        void mosDiodeJunction( double V, double I_S, double N, double * I, double * g ) const;
+        /**
+        * Limits the drain-source voltage to prevent divergence in the
+        * nonlinear iterations.
+        */
+        double fetVoltageDS( double V, double V_prev ) const;
+        /**
+        * Limits the forward voltage to prevent divergence in the nonlinear
+        * iterations.
+        */
+        double fetVoltage( double V, double V_prev, double Vth ) const;
+        
+        double diodeLimitedVoltage( double I_S, double N ) const;
 };
 
 #endif
