8da56fdcd7dc0984a59d373e7359586c387066e2
Zoltan Padrah <zoltan_padrah@users.sf.net>
2011-05-07 23:19:49 +0200

    revert Diode and Nonlinear code to the one at v0.3.7
    This way the diode seems to work correcly

--- src/electronics/simulation/diode.h
+++ src/electronics/simulation/diode.h
@@ -1,29 +1,30 @@
 /***************************************************************************
- *   Copyright (C) 2003-2004 by David Saxton                               *
- *   david@bluehaze.org                                                    *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- ***************************************************************************/
+*   Copyright (C) 2003-2004 by David Saxton                               *
+*   david@bluehaze.org                                                    *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+***************************************************************************/
 
 #ifndef DIODE_H
 #define DIODE_H
 
 #include "nonlinear.h"
 
-class DiodeSettings {
-
-public:
-	DiodeSettings();
-	void reset();
-
-	double I_S;	///< Diode saturation current
-	double N;	///< Emission coefficient
-	double V_B;	///< Reverse breakdown
-	double SR;	///< Series resistance
+class DiodeSettings
+{
+    public:
+        DiodeSettings();
+        void reset();
+        
+        double I_S; ///< Diode saturation current
+        double N;   ///< Emission coefficient
+        double V_B; ///< Reverse breakdown
+        //      double R;   ///< Series resistance
 };
+
 
 /**
 This simulates a diode. The simulated diode characteristics are:
@@ -38,42 +39,34 @@ This simulates a diode. The simulated diode characteristics are:
 @short Simulates the electrical property of diode-ness
 @author David Saxton
 */
-class Diode : public NonLinear {
-
-public:
-	Diode();
-	virtual ~Diode();
-
-	virtual void update_dc();
-	virtual void add_initial_dc();
-	virtual Element::Type type() const {
-		return Element_Diode;
-	}
-
-	DiodeSettings settings() const {
-		return m_diodeSettings;
-	}
-
-	void setDiodeSettings(const DiodeSettings & settings);
-
-    /**
-     * Returns the current flowing through the diode
-     */
-	double current() const { return I_old; }
-
-    virtual void updateCurrents();
-protected:
-	void calc_eq(double *g_new, double *I_new);
-	void calcIg(double V, double *I, double *g) const;
-	void updateLim();
-
-	double g_old, I_old;
-
-	DiodeSettings m_diodeSettings;
-
-	double v_prev, v_lim;
-
+class Diode : public NonLinear
+{
+    public:
+        Diode();
+        virtual ~Diode();
+        
+        virtual void update_dc();
+        virtual void add_initial_dc();
+        virtual Element::Type type() const { return Element_Diode; }
+        DiodeSettings settings() const { return m_diodeSettings; }
+        void setDiodeSettings( const DiodeSettings & settings );
+        /**
+        * Returns the current flowing through the diode
+        */
+        double current() const;
+        
+    protected:
+        virtual void updateCurrents();
+        void calc_eq();
+        void calcIg( double V, double * I, double * g ) const;
+        void updateLim();
+        
+        double g_new, g_old;
+        double I_new, I_old;
+        double V_prev;
+        double V_lim;
+        
+        DiodeSettings m_diodeSettings;
 };
 
 #endif
-
