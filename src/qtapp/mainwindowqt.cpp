8453c28c5b28ed45259dc43b03bc474e24132845
Zoltan P <zoltan.padrah@users.sf.net>
2013-09-28 21:12:25 +0300

    showing a trivial window works

---
+++ src/qtapp/mainwindowqt.cpp
@@ -0,0 +1,34 @@
+/*
+KTechLab, an IDE for electronics
+Copyright (C) 2010 Zoltan Padrah
+    zoltan_padrah@users.sourceforge.net
+
+    This program is free software;
+you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation;
+either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY;
+without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program;
+if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include "mainwindowqt.h"
+
+using namespace KTechLab;
+
+MainWindowQt::MainWindowQt(): QMainWindow()
+{
+	// TODO implement
+}
+
+
