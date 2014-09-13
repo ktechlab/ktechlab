71e6e7d79a295470a090716f190ff3eb5795cba9
Zoltan P <zoltan_padrah@users.sf.net>
2014-09-13 19:38:19 +0300

    add class kcomponent metadata, asa a stup for KDE's KComponentMetadata class; needed by various parts of the code

---
+++ src/interfaces/kcomponentdata.h
@@ -0,0 +1,16 @@
+#if !KDE_ENABLED
+
+#ifndef KCOMPONENTDATA_H
+#define KCOMPONENTDATA_H
+
+
+/** Dummy class used for placeholder for KDE KComponentData */
+class KComponentData
+{
+public:
+    KComponentData();
+};
+
+#endif // KCOMPONENTDATA_H
+
+#endif // !KDE_ENABLED
