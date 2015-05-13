7a163edfea552eb2739469d9642037d4c9bdb4f2
Zoltan P <zoltan_padrah@users.sf.net>
2015-05-13 17:50:29 +0300

    ecnode: move the pin() method to the cpp file, so it the warning it generates does not appear multiple times in the build log

--- src/electronics/ecnode.h
+++ src/electronics/ecnode.h
@@ -60,8 +60,8 @@ class ECNode : public Node
 		 * @param num number of the 
 		 * @return pointer to a pin in this node, given by num
 		 */
-		Pin *pin( unsigned num = 0 ) const
-			{ return (num < m_pins.size()) ? m_pins[num] : 0l; }
+		Pin *pin( unsigned num = 0 ) const ;
+			//{ return (num < m_pins.size()) ? m_pins[num] : 0l; }
 
 		bool showVoltageBars() const { return m_bShowVoltageBars; }
 		void setShowVoltageBars( bool show ) { m_bShowVoltageBars = show; }
