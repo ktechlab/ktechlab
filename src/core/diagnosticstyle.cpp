06738805c4e31a612060f694aef40adc97c135e0
Zoltan P <zoltan_padrah@users.sf.net>
2015-05-16 14:55:48 +0300

    main: add diagnosticstyle class for inspecting widget layout in ktechlab. useful for debugging strange widget behavior

---
+++ src/core/diagnosticstyle.cpp
@@ -0,0 +1,37 @@
+/***************************************************************************
+ *   Copyright (C) 2015 Zoltan Padrah                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#include "diagnosticstyle.h"
+
+#include <Qt/qpainter.h>
+#include <Qt/qwidget.h>
+
+void DiagnosticStyle::drawControl(ControlElement element, const QStyleOption* option, QPainter* painter, const QWidget* widget) const
+{
+    BaseStyle::drawControl(element, option, painter, widget);
+    if (widget && painter) {
+        // draw a border around the widget
+        painter->setPen(QColor("red"));
+        painter->drawRect(widget->rect());
+
+        // show the classname of the widget
+        QBrush translucentBrush(QColor(255,246,240, 100));
+        painter->fillRect(widget->rect(), translucentBrush);
+        painter->setPen(QColor("darkblue"));
+//         QFont textFont = painter->font();
+//         textFont.setPointSize( 8 );
+//         painter->setFont(textFont);
+        painter->drawText(widget->rect(),
+                          Qt::AlignLeft | Qt::AlignTop,
+                          //Qt::AlignRight | Qt::AlignBottom,
+                          widget->metaObject()->className());
+    }
+}
+
+#include "diagnosticstyle.moc"
---
+++ src/core/diagnosticstyle.h
@@ -0,0 +1,29 @@
+/***************************************************************************
+ *   Copyright (C) 2015 Zoltan Padrah                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#ifndef KTECHLAB_CORE_MAIN_H__
+#define KTECHLAB_CORE_MAIN_H__
+
+#include <QtGui/QMotifStyle>
+
+/**
+ * see approach from here:
+ * http://stackoverflow.com/questions/5909907/drawing-an-overlay-on-top-of-an-applications-window
+ */
+class DiagnosticStyle : public QMotifStyle
+{
+Q_OBJECT
+
+public:
+    typedef QMotifStyle BaseStyle;
+    void drawControl(ControlElement element, const QStyleOption* option, QPainter* painter, const QWidget* widget) const;
+    virtual ~DiagnosticStyle() { }
+};
+
+#endif // KTECHLAB_CORE_MAIN_H__
