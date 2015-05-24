4d22a589a9a80462c03479ea2d1eccbcde484e1b
Zoltan P <zoltan_padrah@users.sf.net>
2015-05-24 19:01:45 +0300

    gpsim detection: fix compilation when gpsim >= 0.26. A member has been renamed in gpsim's pic_processor

--- cmake/modules/FindGPSim.cmake
+++ cmake/modules/FindGPSim.cmake
@@ -1,3 +1,14 @@
+# CMake module to find GPSim
+#
+# possibly defined variables after this script is run:
+# GPSIM_FOUND
+# HAVE_GPSIM_0_26		# TODO create a GPSIM_VERSION variable, and use that
+# HAVE_GPSIM_0_21_4
+# HAVE_GPSIM_0_21_12
+# HAVE_GPSIM_0_21_11
+# NO_GPSIM
+#
+# start by looking for 'curses' library
 
 FIND_PATH( GPSim_INCLUDE_DIR gpsim_interface.h /usr/include/gpsim /usr/local/include/gpsim )
 
@@ -5,76 +16,124 @@ FIND_LIBRARY( GPSim_LIBRARY NAMES gpsim PATH /usr/lib /usr/local/lib )
 
 if( GPSim_INCLUDE_DIR AND GPSim_LIBRARY )
 	set( GPSim_FOUND TRUE )
+elseif( GPSim_INCLUDE_DIR AND GPSim_LIBRARY )
+	message(STATUS "No GPSim found; files not present")
+	SET( NO_GPSIM "No GPSim found; files not present" )
+	return()
 endif( GPSim_INCLUDE_DIR AND GPSim_LIBRARY )
 
-if ( GPSim_FOUND )
-	INCLUDE( CheckCXXSourceCompiles )
 
-	set( CMAKE_REQUIRED_INCLUDES
+INCLUDE( CheckCXXSourceCompiles )
+INCLUDE( CheckIncludeFileCXX )
+
+set( CMAKE_REQUIRED_INCLUDES
+	${GLIB2_INCLUDE_DIRS}
+	)
+set( CMAKE_REQUIRED_FLAGS
+	-fexceptions
+	)
+
+# HAVE_GPSIM_0_26
+
+CHECK_CXX_SOURCE_COMPILES(
+		"#include <gpsim/pic-processor.h>
+		int main() { pic_processor *proc = NULL;
+		return sizeof (proc->Wreg);
+		}"
+		HAVE_GPSIM_0_26 )
+
+if( HAVE_GPSIM_0_26 )
+	message(STATUS "GPSim 0.26 found")
+	set( GPSIM_0_26 "GPSim 0.26 found" )
+	set( GPSIM_INCLUDE_DIRS
+		${GPSim_INCLUDE_DIR}
 		${GLIB2_INCLUDE_DIRS}
 		)
-	set( CMAKE_REQUIRED_FLAGS
-		-fexceptions
+	set( GPSIM_LIBRARY
+		${GPSim_LIBRARY}
+		${GLIB2_LIBRARY}
 		)
+	set( GPSIM_FOUND TRUE )
+	return()
+endif( HAVE_GPSIM_0_26 )
 
-	CHECK_CXX_SOURCE_COMPILES(
-        	"#include <gpsim/interface.h>
-        	#include <gpsim/gpsim_interface.h>
-        	#include <gpsim/breakpoints.h>
-        	#include <gpsim/gpsim_time.h>
-        	void func() { (void)cycles; (void)initialize_gpsim_core();
-        	(void)load_symbol_file(0,0); }" HAVE_GPSIM_0_21_4 )
 
-	CHECK_CXX_SOURCE_COMPILES(
-        	"#include <gpsim/interface.h>
+# HAVE_GPSIM_0_21_12
+
+CHECK_INCLUDE_FILE_CXX( ${GPSim_INCLUDE_DIR}/ValueCollections.h HAVE_GPSIM_0_21_12 )
+
+if( HAVE_GPSIM_0_21_12 )
+	message( STATUS "GPSim 0.21.12 found" )
+	set( GPSIM_0_21_12 "GPSim 0.21.12 found" )
+	set( GPSIM_INCLUDE_DIRS
+		${GPSim_INCLUDE_DIR}
+		${GLIB2_INCLUDE_DIRS}
+		)
+	set( GPSIM_LIBRARY
+		${GPSim_LIBRARY}
+		${GLIB2_LIBRARY}
+		)
+	set( GPSIM_FOUND TRUE )
+	return()
+endif( HAVE_GPSIM_0_21_12 )
+
+
+# HAVE_GPSIM_0_21_4
+
+CHECK_CXX_SOURCE_COMPILES(
+		"#include <gpsim/interface.h>
 		#include <gpsim/gpsim_interface.h>
-        	#include <gpsim/breakpoints.h>
-        	#include <gpsim/gpsim_time.h>
-        	#include <gpsim/symbol.h>
-        	#include <gpsim/program_files.h>
-        	void func() { (void)cycles; (void)initialize_gpsim_core(); }"
-        	HAVE_GPSIM_0_21_11 )
+		#include <gpsim/breakpoints.h>
+		#include <gpsim/gpsim_time.h>
+		void func() { (void)cycles; (void)initialize_gpsim_core();
+		(void)load_symbol_file(0,0); }" HAVE_GPSIM_0_21_4 )
 
-	INCLUDE( CheckIncludeFileCXX )
 
-	CHECK_INCLUDE_FILE_CXX( ${GPSim_INCLUDE_DIR}/ValueCollections.h HAVE_GPSIM_0_21_12 )
+if( HAVE_GPSIM_0_21_4 )
+	message( STATUS "GPSim 0.21.4 found" )
+	set( GPSIM_0_21_4 "GPSim 0.21.4 found" )
+	set( GPSIM_INCLUDE_DIRS
+		${GPSim_INCLUDE_DIR}
+		${GLIB2_INCLUDE_DIRS}
+		)
+	set( GPSIM_LIBRARY
+		${GPSim_LIBRARY}
+		${GLIB2_LIBRARY}
+		)
+	set( GPSIM_FOUND TRUE )
+	return()
+endif( HAVE_GPSIM_0_21_4 )
 
-	if( HAVE_GPSIM_0_21_12 )
-        	message( STATUS "GPSim 0.21.12 found" )
-        	set( GPSIM_0_21_12 "GPSim 0.21.12 found" )
-	else( HAVE_GPSIM_0_21_12 )
-        	if( HAVE_GPSIM_0_21_4 )
-                	message( STATUS "GPSim 0.21.4 found" )
-                	set( GPSIM_0_21_4 "GPSim 0.21.4 found" )
-        	else( HAVE_GPSIM_0_21_4 )
-                	if( HAVE_GPSIM_0_21_11 )
-                        	message( STATUS "GPSim 0.21.11 found" )
-                        	set( GPSIM_0_21_11 "GPSim 0.21.11 found" )
-                	else( HAVE_GPSIM_0_21_11 )
-                        	message( STATUS "No GPSim found" )
-                        	set( NO_GPSIM "No GPSim found" )
-                	endif( HAVE_GPSIM_0_21_11 )
-        	endif( HAVE_GPSIM_0_21_4 )
-	endif( HAVE_GPSIM_0_21_12 )
 
-# removed check on readline library, because (1) it is broken (2) make module finding simpler
-#	INCLUDE( CheckLibraryExists )
-#	CHECK_LIBRARY_EXISTS( readline readline /usr/lib HAVE_LIBREADLINE )
+# HAVE_GPSIM_0_21_11
 
-	find_package( Curses )
+CHECK_CXX_SOURCE_COMPILES(
+		"#include <gpsim/interface.h>
+		#include <gpsim/gpsim_interface.h>
+		#include <gpsim/breakpoints.h>
+		#include <gpsim/gpsim_time.h>
+		#include <gpsim/symbol.h>
+		#include <gpsim/program_files.h>
+		void func() { (void)cycles; (void)initialize_gpsim_core(); }"
+		HAVE_GPSIM_0_21_11 )
 
-#	if( HAVE_LIBREADLINE )
-		set( GPSIM_INCLUDE_DIRS 
-			${GPSim_INCLUDE_DIR}
-			${GLIB2_INCLUDE_DIRS}
-			)
-		set( GPSIM_LIBRARY
-#			-lreadline
-			${GPSim_LIBRARY}
-			${GLIB2_LIBRARY}
-			)
-		set( GPSIM_FOUND TRUE )
-#	else( HAVE_LIBREADLINE )
+if( HAVE_GPSIM_0_21_11 )
+	message( STATUS "GPSim 0.21.11 found" )
+	set( GPSIM_0_21_11 "GPSim 0.21.11 found" )
+	set( GPSIM_INCLUDE_DIRS
+		${GPSim_INCLUDE_DIR}
+		${GLIB2_INCLUDE_DIRS}
+		)
+	set( GPSIM_LIBRARY
+		${GPSim_LIBRARY}
+		${GLIB2_LIBRARY}
+		)
+	set( GPSIM_FOUND TRUE )
+	return()
+endif( HAVE_GPSIM_0_21_11 )
 
-#	endif( HAVE_LIBREADLINE )
-endif( GPSim_FOUND)
+
+# any other case
+
+message( STATUS "No GPSim found; unknown version?" )
+set( NO_GPSIM "No GPSim found; unknown version?" )
