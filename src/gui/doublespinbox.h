97c4f8a7594fc4eeca0fb17f2b2d5db72c1be30f
Zoltan Padrah <zoltan_padrah@users.sf.net>
2015-07-04 23:02:49 +0300

    doublespinbox: re-adapt the interface to the QDoubleSpinBox class, and simplify/remove code. When items are clicked, their properties' values should be preserved now, cand the properties should be correctly editable.

--- src/gui/doublespinbox.h
+++ src/gui/doublespinbox.h
@@ -20,7 +20,7 @@ spin box plus the SI magnitude symbol it is showing
 
 @author David Saxton
 */
-class DoubleSpinBox : public QSpinBox
+class DoubleSpinBox : public QDoubleSpinBox
 {
 	Q_OBJECT
 	public:
@@ -31,27 +31,27 @@ class DoubleSpinBox : public QSpinBox
 		/**
 		 * The minimum value is the lowest number that the user can enter.
 		 */
-		double minValue() const { return m_minValue; }
+// 		double minValue() const { return m_minValue; }
 		/**
 		 * @see minValue
 		 */
-		void setMinValue( double minValue ) { m_minValue = minValue; }
+// 		void setMinValue( double minValue ) { m_minValue = minValue; }
 		/**
 		 * The minimum value is the lowest number that the user can enter.
 		 */
-		void setMinValue( int minValue ) { m_minValue = minValue; }
+// 		void setMinValue( int minValue ) { m_minValue = minValue; }
 		/**
 		 * The maximum value is the highest number that the user can enter.
 		 */
-		double maxValue() const { return m_maxValue; }
+// 		double maxValue() const { return m_maxValue; }
 		/**
 		 * @see maxValue
 		 */
-		void setMaxValue( double maxValue ) { m_maxValue = maxValue; }
+// 		void setMaxValue( double maxValue ) { m_maxValue = maxValue; }
 		/**
 		 * @see maxValue
 		 */
-		void setMaxValue( int maxValue ) { m_maxValue = maxValue; }
+// 		void setMaxValue( int maxValue ) { m_maxValue = maxValue; }
 		/**
 		 * The minimum absolute value is the smallest value that the user can
 		 * enter before the value is considered 0.
@@ -61,72 +61,88 @@ class DoubleSpinBox : public QSpinBox
 		 * The actual value that the user has entered - e.g. if the spinbox
 		 * displays "100 kF", then the value returned will be 1e5.
 		 */
-		double value();
+// 		double value();
 		/**
 		 * Sets the unit used, e.g. "F"
 		 */
-		void setUnit( const QString & unit );
+// 		void setUnit( const QString & unit );
 		
+		virtual QValidator::State validate( QString & text, int & pos ) const ;
+
 	public slots:
-		virtual void stepUp();
-		virtual void stepDown();
+// 		virtual void stepUp(); // QDoubleSpinBox has these
+// 		virtual void stepDown();
 		/**
 		 * Set the value to be displayed - e.g. if value is 1e5, then the
 		 * spinbox might display "100 kF".
 		 */
-		void setValue( double value );
+// 		void setValue( double value );
 	
 	signals:
 		/**
 		 * This value is emitted whenever the value of the spinbox changes.
 		 */
-		void valueChanged( double value );
+// 		void valueChanged( double value ); // exists in QDoubleSpinBox
 		
 	protected slots:
 		/**
 		 * Checks if the value has changed - and if so, emits a valueChanged
 		 * signal.
 		 */
-		void checkIfChanged();
+// 		void checkIfChanged(double value);
 		/**
 		 * Sets the suffix from m_queuedSuffix. Called from QTimer::singleShot 
 		 * to avoid strange recursion problems.
 		 */
-		void setQueuedSuffix();
+// 		void setQueuedSuffix();
 	
 	protected:
+        /**
+         * make Qt enable the up/down step arrows
+         */
+        virtual StepEnabled stepEnabled () const ;
+        /**
+         * Change the value of the spin box, because of user interaction
+         */
+        virtual void stepBy( int steps );
+
+        double getNextUpStepValue( double in );
+        double getNextDownStepValue( double in );
+
 		/**
 		 * Updates the suffix using m_unit and value.
 		 */
-		void updateSuffix( double value );
+ 		void updateSuffix( double value );
 		/**
 		 * Returns the multiplication number from what is displayed
 		 * in the box, e.g. "10 kV" will return "1000" due to the letter "k" presence
 		 */
-		double getMult();
+// 		double getMult();
 		/**
 		 * Returns the number currently displayed in the spin box.
 		 */
-		double getDisplayedNumber( bool * ok );
+// 		double getDisplayedNumber( bool * ok );
 		/**
 		 *  Overloaded the method in QSpinxBox to allow SI prefixes to be entered
 		 */
-		virtual int mapTextToValue( bool * ok );
+// 		virtual int mapTextToValue( bool * ok );
+        virtual double valueFromText( const QString & text ) const ;
 		/**
 		 *  Overloaded the method in QSpinxBox to allow SI prefixes to be entered
 		 */
-		virtual QString mapValueToText( int v );
+// 		virtual QString mapValueToText( int v );
+        virtual QString textFromValue ( double value ) const ;
 		/**
 		 * Returns value rounded off to one significant figure.
 		 */
-		double roundToOneSF( double value );
+		static double roundToOneSF( double value );
 		
 		void init();
 		
-		QString m_queuedSuffix; ///< Used
+// 		QString m_queuedSuffix; ///< Used
 		QString m_unit;
-		double m_minValue;
-		double m_maxValue;
+// 		double m_minValue;
+// 		double m_maxValue;
 		double m_minAbsValue;
 		double m_lastEmittedValue;
 };
