9953086498f415c2820397517ea7b2b072505666
Zoltan Padrah <zoltan_padrah@users.sf.net>
2015-10-30 18:04:58 +0200

    ecclockinput: review the code, clean it up and document it; part of fixing the bug of strange clock behavior. The issue probably has been caused by graphics drivers and graphics system, so stopping investigation.

--- src/electronics/components/ecclockinput.h
+++ src/electronics/components/ecclockinput.h
@@ -34,8 +34,13 @@ public:
 	static Item* construct( ItemDocument *itemDocument, bool newItem, const char *id );
 	static LibraryItem *libraryItem();
 	
+    /** callback for logic steps, for each logic update step;
+     only active when the period of the clock is less or equal than LOGIC_UPDATE_PER_STEP */
 	void stepCallback();
+    /** callback for linear steps, for each linear update step;
+     only active when the period of the clock is greater than LOGIC_UPDATE_PER_STEP */
 	void stepLogic();
+    /** callback at linear steps; always active */
 	virtual void stepNonLogic();
 	virtual bool doesStepNonLogic() const { return true; }
 	
@@ -44,15 +49,19 @@ protected:
 	void dataChanged();
 	
 	uint m_time;
+    /** unit: simulator logic update tick == 1s / LOGIC_UPDATE_RATE */
 	uint m_high_time;
+    /** unit: simulator logic update tick == 1s / LOGIC_UPDATE_RATE */
 	uint m_low_time;
+    /** unit: simulator logic update tick == 1s / LOGIC_UPDATE_RATE */
 	uint m_period;
+    /** unit: simulator logic update tick == 1s / LOGIC_UPDATE_RATE */
 	long long m_lastSetTime;
 	LogicOut * m_pOut;
 	bool m_bSetStepCallbacks;
 	bool m_bLastStepCallbackOut;
 	Simulator * m_pSimulator;
-	std::list<ComponentCallback> * m_pComponentCallback[1000];
+	std::list<ComponentCallback> * m_pComponentCallback[ 100 /* == LOGIC_UPDATE_PER_STEP */ ];
 };
 
 #endif
