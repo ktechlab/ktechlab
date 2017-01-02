396d25a49c386c6c80f6077d9acf005fb854164d
Pino Toscano <pino@kde.org>
2017-01-02 12:12:23 +0100

    cmake: drop unused HAVE_GPSIM_x variables
    basically only HAVE_GPSIM_0_26 is used atm

--- cmake/modules/FindGPSim.cmake
+++ cmake/modules/FindGPSim.cmake
@@ -3,9 +3,6 @@
 # possibly defined variables after this script is run:
 # GPSIM_FOUND
 # HAVE_GPSIM_0_26		# TODO create a GPSIM_VERSION variable, and use that
-# HAVE_GPSIM_0_21_4
-# HAVE_GPSIM_0_21_12
-# HAVE_GPSIM_0_21_11
 # NO_GPSIM
 #
 # start by looking for 'curses' library
@@ -56,81 +53,6 @@ if( HAVE_GPSIM_0_26 )
 	set( GPSIM_FOUND TRUE )
 	return()
 endif( HAVE_GPSIM_0_26 )
-
-
-# HAVE_GPSIM_0_21_12
-
-CHECK_INCLUDE_FILE_CXX( ${GPSim_INCLUDE_DIR}/ValueCollections.h HAVE_GPSIM_0_21_12 )
-
-if( HAVE_GPSIM_0_21_12 )
-	message( STATUS "GPSim 0.21.12 found" )
-	set( GPSIM_0_21_12 "GPSim 0.21.12 found" )
-	set( GPSIM_INCLUDE_DIRS
-		${GPSim_INCLUDE_DIR}
-		${GLIB2_INCLUDE_DIRS}
-		)
-	set( GPSIM_LIBRARY
-		${GPSim_LIBRARY}
-		${GLIB2_LIBRARY}
-		)
-	set( GPSIM_FOUND TRUE )
-	return()
-endif( HAVE_GPSIM_0_21_12 )
-
-
-# HAVE_GPSIM_0_21_4
-
-CHECK_CXX_SOURCE_COMPILES(
-		"#include <gpsim/interface.h>
-		#include <gpsim/gpsim_interface.h>
-		#include <gpsim/breakpoints.h>
-		#include <gpsim/gpsim_time.h>
-		void func() { (void)cycles; (void)initialize_gpsim_core();
-		(void)load_symbol_file(0,0); }" HAVE_GPSIM_0_21_4 )
-
-
-if( HAVE_GPSIM_0_21_4 )
-	message( STATUS "GPSim 0.21.4 found" )
-	set( GPSIM_0_21_4 "GPSim 0.21.4 found" )
-	set( GPSIM_INCLUDE_DIRS
-		${GPSim_INCLUDE_DIR}
-		${GLIB2_INCLUDE_DIRS}
-		)
-	set( GPSIM_LIBRARY
-		${GPSim_LIBRARY}
-		${GLIB2_LIBRARY}
-		)
-	set( GPSIM_FOUND TRUE )
-	return()
-endif( HAVE_GPSIM_0_21_4 )
-
-
-# HAVE_GPSIM_0_21_11
-
-CHECK_CXX_SOURCE_COMPILES(
-		"#include <gpsim/interface.h>
-		#include <gpsim/gpsim_interface.h>
-		#include <gpsim/breakpoints.h>
-		#include <gpsim/gpsim_time.h>
-		#include <gpsim/symbol.h>
-		#include <gpsim/program_files.h>
-		void func() { (void)cycles; (void)initialize_gpsim_core(); }"
-		HAVE_GPSIM_0_21_11 )
-
-if( HAVE_GPSIM_0_21_11 )
-	message( STATUS "GPSim 0.21.11 found" )
-	set( GPSIM_0_21_11 "GPSim 0.21.11 found" )
-	set( GPSIM_INCLUDE_DIRS
-		${GPSim_INCLUDE_DIR}
-		${GLIB2_INCLUDE_DIRS}
-		)
-	set( GPSIM_LIBRARY
-		${GPSim_LIBRARY}
-		${GLIB2_LIBRARY}
-		)
-	set( GPSIM_FOUND TRUE )
-	return()
-endif( HAVE_GPSIM_0_21_11 )
 
 
 # any other case
