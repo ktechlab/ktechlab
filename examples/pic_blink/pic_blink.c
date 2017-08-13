dc9eac415dac91af52cc9bc746a5bfef0f29b166
Zoltan Padrah <zoltan_padrah@users.sf.net>
2017-08-13 16:45:19 +0300

    example: pic blink: add license comment
    should fix the license warning from KDE infrastructure

--- examples/pic_blink/pic_blink.c
+++ examples/pic_blink/pic_blink.c
@@ -3,6 +3,23 @@
 // Copyright (C) 2014 Diego Herranz
 // GPLv2
 //
+/*
+
+Copyright (C) 2014 Diego Herranz
+
+This file is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <pic16regs.h>
 #include <stdint.h>
