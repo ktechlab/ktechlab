8e52be611d78b5274d46d5cdae2fca938f51606e
Zoltan Padrah <zoltan_padrah@users.sf.net>
2017-09-24 21:20:53 +0300

    update about data, for Help -> About KTechLab
    update:
    - copyright year,
    - website to point to a reasonable location
    - mailing list at KDE
    - authors/credits synced with initial KDE4 port

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -35,19 +35,44 @@ int main(int argc, char **argv)
     LogToFileMsgHandler logFileHandler;
 
     KAboutData about(QByteArray("ktechlab"), QByteArray("ktechlab"), ki18n("KTechLab"), VERSION, ki18n(description),
-                    KAboutData::License_GPL, ki18n("(C) 2003-2009, The KTechLab developers"),
-                    KLocalizedString(), "http://ktechlab.org", "ktechlab-devel@lists.sourceforge.net" );
-	about.addAuthor( ki18n("Alan Grimes"), ki18n("Developer"), "" );
-	about.addAuthor( ki18n("Zoltan Padrah"), ki18n("Developer") , QByteArray("zoltan_padrah@users.sourceforge.net"));
-	about.addCredit( ki18n("Jason Lucas"), ki18n("Keeping the project up as a maintainer during the time David left"), "" );
-	about.addCredit( ki18n("Lawrence Shafer"), ki18n("Website, wiki and forum"), "" );
-	about.addCredit( ki18n("Julian Bäume"), ki18n("Some bug-fixes"), QByteArray("julian@svg4all.de") );
-	about.addCredit( ki18n("David Saxton"), ki18n("Former developer, project founder, former maintainer"), QByteArray("david@bluehaze.org") );
-	about.addCredit( ki18n("Daniel Clarke"), ki18n("Former developer"), QByteArray("daniel.jc@gmail.com") );
-	about.addCredit( ki18n("Couriousous"), ki18n("JK flip-flop, asynchronous preset/reset in the D flip-flop"), "" );
-	about.addCredit( ki18n("John Myers"), ki18n("Rotary Switch"), "" );
-	about.addCredit( ki18n("Ali Akcaagac"), ki18n("Glib friendliness"), "" );
-	about.addCredit( ki18n("David Leggett"), ki18n("Former website hosting and feedback during early development"), "" );
+                    KAboutData::License_GPL, ki18n("(C) 2003-2017, The KTechLab developers"),
+                    KLocalizedString(), "https://github.com/KDE/ktechlab/", "ktechlab-devel@kde.org" );
+	about.addAuthor( ki18n("Alan Grimes"),
+                     ki18n("Developer, Simulation"),
+                     "" );
+	about.addAuthor( ki18n("Zoltan Padrah"),
+                     ki18n("Developer") ,
+                     QByteArray("zoltan_padrah@users.sourceforge.net"));
+    about.addAuthor( ki18n("Julian Bäume"),
+                     ki18n("Developer, KDE4 Port, GUI"),
+                     QByteArray("julian@svg4all.de") );
+    about.addAuthor( ki18n("Juan De Vincenzo"),
+                     ki18n("KDE4 Port"),
+                     "");
+    about.addCredit( ki18n("Lawrence Shafer"),
+                     ki18n("Website, wiki and forum"),
+                     "");
+	about.addCredit( ki18n("Jason Lucas"),
+                     ki18n("Keeping up the project during lack of developers"),
+                     "" );
+	about.addCredit( ki18n("David Saxton"),
+                     ki18n("Former developer, project founder, former maintainer"),
+                     QByteArray("david@bluehaze.org") );
+	about.addCredit( ki18n("Daniel Clarke"),
+                     ki18n("Former developer"),
+                     QByteArray("daniel.jc@gmail.com") );
+	about.addCredit( ki18n("Couriousous"),
+                     ki18n("JK flip-flop, asynchronous preset/reset in the D flip-flop"),
+                     "" );
+	about.addCredit( ki18n("John Myers"),
+                     ki18n("Rotary Switch"),
+                     "" );
+	about.addCredit( ki18n("Ali Akcaagac"),
+                     ki18n("Glib friendliness"),
+                     "" );
+	about.addCredit( ki18n("David Leggett"),
+                     ki18n("Former website hosting and feedback during early development"),
+                     "" );
 
     KCmdLineArgs::init(argc, argv, &about);
     KCmdLineOptions options;
