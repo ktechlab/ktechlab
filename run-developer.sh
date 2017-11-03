4584022580c7cb22db129ad46a0ea4040607825e
Zoltan Padrah <zoltan_padrah@users.sf.net>
2017-11-03 23:50:57 +0200

    buildsystem: run developer: ASAN do not warn on mismatched delete
    because kde libs are very often triggering it

--- run-developer.sh
+++ run-developer.sh
@@ -6,4 +6,6 @@ SCRIPTDIR=$(dirname $(readlink -f "$0"))
 
 export PATH="$SCRIPTDIR/inst-developer/bin:$PATH"
 
+export ASAN_OPTIONS=new_delete_type_mismatch=0
+
 "$SCRIPTDIR/inst-developer/bin/ktechlab" 2>&1 | tee "$SCRIPTDIR/developer-run.log"
