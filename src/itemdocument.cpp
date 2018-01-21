188fd14b1d313fb5e30bdc7c85a335fcd487c95f
Cheuk Yin Ng <chucksys88@gmail.com>
2018-01-21 07:46:49 -0800

    fix overwrite text for saving and exporting

--- src/itemdocument.cpp
+++ src/itemdocument.cpp
@@ -867,9 +867,7 @@ void ItemDocument::exportToImage()
 		int query = KMessageBox::warningYesNo(
             KTechlab::self(),
             i18n( "A file named \"%1\" already exists. " "Are you sure you want to overwrite it?", url.fileName() ),
-            i18n( "Overwrite File?" ),
-            //i18n( "Overwrite" ),
-            KStandardGuiItem::cancel() );
+            i18n( "Overwrite File?" ));
 
 		if ( query == KMessageBox::No ) return;
 	}
