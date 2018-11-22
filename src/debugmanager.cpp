a8c59ffa552fb3602e066b10e4ecde782c7127ed
Zoltan Padrah <zoltan_padrah@users.sf.net>
2018-11-22 18:49:52 +0200

    debug manager: replace QList::remove() with removeAll()

--- src/debugmanager.cpp
+++ src/debugmanager.cpp
@@ -69,7 +69,7 @@ void DebugManager::urlOpened( TextDocument * td )
 	if ( td->debuggerIsRunning() )
 		return;
 	
-	m_processors.remove( (GpsimProcessor*)0l );
+	m_processors.removeAll( (GpsimProcessor*)0l );
 	GpsimProcessorList::iterator end = m_processors.end();
 	for ( GpsimProcessorList::iterator it = m_processors.begin(); it != end; ++it )
 	{
