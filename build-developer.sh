fb59bc0bb988b3e387548a0b178d1b5d708b9be6
Zoltan Padrah <zoltan_padrah@users.sf.net>
2018-11-24 03:55:38 +0200

    run-simple, build-developer, run-developer: work on OS X, too
    copy the fix from build-simple.sh to the other scripts
    
    this way the scripts are now consistent between each other
    
    cannot extract this code in a separate file, because that should be
    sourced from a directory, which could be different from the current
    working directory

--- build-developer.sh
+++ build-developer.sh
@@ -5,7 +5,35 @@ set -x
 # script for building ktechlab for development/testing
 # currently it (1) builds ktechlab with gcc-5 and (2) enables address sanitizer
 
-SCRIPTDIR=$(dirname $(readlink -f "$0"))
+#SCRIPTDIR=$(dirname $(readlink -f "$0"))
+
+case "$(uname -s)" in
+
+   Darwin)
+     echo 'Mac OS X'
+     readlinkf(){ perl -MCwd -e 'print Cwd::abs_path shift' "$1";}
+     SCRIPTDIR=$(dirname $(readlinkf "$0"))
+     ;;
+
+   # taken from: https://en.wikipedia.org/wiki/Uname
+   Linux|*BSD)
+     echo 'Linux'
+     SCRIPTDIR=$(dirname $(readlink -f "$0"))
+     ;;
+
+   CYGWIN*|MINGW32*|MSYS*)
+     echo 'MS Windows'
+     SCRIPTDIR=$(dirname $(readlink -f "$0")) # untested
+     ;;
+
+   # Add here more strings to compare
+   #
+
+   *)
+     echo 'other OS, please notify the developers'
+     SCRIPTDIR=$(dirname $(readlink -f "$0")) # untested
+     ;;
+esac
 
 LOGFILE="$SCRIPTDIR/build-developer.log"
 
--- run-developer.sh
+++ run-developer.sh
@@ -2,7 +2,35 @@
 set -e
 set -x
 
-SCRIPTDIR=$(dirname $(readlink -f "$0"))
+#SCRIPTDIR=$(dirname $(readlink -f "$0"))
+
+case "$(uname -s)" in
+
+   Darwin)
+     echo 'Mac OS X'
+     readlinkf(){ perl -MCwd -e 'print Cwd::abs_path shift' "$1";}
+     SCRIPTDIR=$(dirname $(readlinkf "$0"))
+     ;;
+
+   # taken from: https://en.wikipedia.org/wiki/Uname
+   Linux|*BSD)
+     echo 'Linux'
+     SCRIPTDIR=$(dirname $(readlink -f "$0"))
+     ;;
+
+   CYGWIN*|MINGW32*|MSYS*)
+     echo 'MS Windows'
+     SCRIPTDIR=$(dirname $(readlink -f "$0")) # untested
+     ;;
+
+   # Add here more strings to compare
+   #
+
+   *)
+     echo 'other OS, please notify the developers'
+     SCRIPTDIR=$(dirname $(readlink -f "$0")) # untested
+     ;;
+esac
 
 export PATH="$SCRIPTDIR/inst-developer/bin:$PATH"
 
