6958eb6f1f7d0138d72a6d39b461237b4eb99306
Zoltan Padrah <zoltan_padrah@users.sf.net>
2019-05-06 15:48:00 +0300

    canvas item parts: fix warning about deprecated method
    it spams the program output

--- src/canvasitemparts.cpp
+++ src/canvasitemparts.cpp
@@ -227,7 +227,8 @@ void Widget::posChanged()
 void Widget::drawShape( QPainter &p )
 {
 // 	initPainter(p);
-	p.drawPixmap( int(x()), int(y()), QPixmap::grabWidget( widget() ) );
+	//p.drawPixmap( int(x()), int(y()), QPixmap::grabWidget( widget() ) ); // 2019.05.06
+    p.drawPixmap( int(x()), int(y()), widget()->grab() );
 // 	deinitPainter(p);
 }
 //END Class Widget
