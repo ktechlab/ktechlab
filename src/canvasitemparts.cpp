ktechlab/ktechlab

commit 5cd465d656712e2c5fefedd7e04b65ec7ca1701f

canvas: convert most connect/disconnect to new syntax

src/canvasitemparts.cpp
@@ -30,7 +30,7 @@ GuiPart::GuiPart(CNItem *parent, const QRect &r, KtlQCanvas *canvas)
     , b_pointsAdded(false)
     , m_originalRect(r)
 {
-    connect(parent, SIGNAL(movedBy(double, double)), this, SLOT(slotMoveBy(double, double)));
+    connect(parent, &CNItem::moveBy, this, &GuiPart::slotMoveBy);
     setZ(parent->z() + 0.5);
 }
 
@@ -257,8 +257,8 @@ Button::Button(const QString &id, CNItem *parent, bool isToggle, const QRect &r,
     m_button = new ToolButton(nullptr);
     m_button->setToolButtonStyle(Qt::ToolButtonIconOnly);
     m_button->setCheckable(b_isToggle);
-    connect(m_button, SIGNAL(pressed()), this, SLOT(slotStateChanged()));
-    connect(m_button, SIGNAL(released()), this, SLOT(slotStateChanged()));
+    connect(m_button, &ToolButton::pressed, this, &Button::slotStateChanged);
+    connect(m_button, &ToolButton::pressed, this, &Button::slotStateChanged);
     posChanged();
 }
 
@@ -435,7 +435,7 @@ Slider::Slider(const QString &id, CNItem *parent, const QRect &r, KtlQCanvas *ca
     QPalette p;
     p.setColor(m_slider->backgroundRole(), Qt::white);
     m_slider->setPalette(p);
-    connect(m_slider, SIGNAL(valueChanged(int)), this, SLOT(slotValueChanged(int)));
+    connect(m_slider, &SliderWidget::valueChanged, this, &Slider::slotValueChanged);
     posChanged();
 }
 

src/canvas.cpp
@@ -438,7 +438,7 @@ void KtlQCanvas::setUpdatePeriod(int ms)
         if (update_timer)
             delete update_timer;
         update_timer = new QTimer(this);
-        connect(update_timer, SIGNAL(timeout()), this, SLOT(update()));
+        connect(update_timer, &QTimer::timeout, this, &KtlQCanvas::update);
         update_timer->start(ms);
     }
 }
@@ -1242,7 +1242,7 @@ KtlQCanvasView::KtlQCanvasView(QWidget *parent, Qt::WindowFlags f)
     d = new KtlQCanvasViewData;
     viewing = nullptr;
     setCanvas(nullptr);
-    connect(this, SIGNAL(contentsMoving(int, int)), this, SLOT(cMoving(int, int)));
+    connect(this, &KtlQCanvasView::contentsMoving, this, &KtlQCanvasView::cMoving);
 }
 
 KtlQCanvasView::KtlQCanvasView(KtlQCanvas *canvas, QWidget *parent, Qt::WindowFlags f)
@@ -1253,7 +1253,7 @@ KtlQCanvasView::KtlQCanvasView(KtlQCanvas *canvas, QWidget *parent, Qt::WindowFl
     viewing = nullptr;
     setCanvas(canvas);
 
-    connect(this, SIGNAL(contentsMoving(int, int)), this, SLOT(cMoving(int, int)));
+    connect(this, &KtlQCanvasView::contentsMoving, this, &KtlQCanvasView::cMoving);
 }
 
 KtlQCanvasView::~KtlQCanvasView()
@@ -1271,7 +1271,7 @@ void KtlQCanvasView::setCanvas(KtlQCanvas *canvas)
     }
     viewing = canvas;
     if (viewing) {
-        connect(viewing, SIGNAL(resized()), this, SLOT(updateContentsSize()));
+        connect(viewing, &KtlQCanvas::resized, this, &KtlQCanvasView::updateContentsSize);
         viewing->addView(this);
     }
     if (d) // called by d'tor

src/canvasmanipulator.cpp
@@ -51,7 +51,7 @@ CMManager::CMManager(ItemDocument *itemDocument)
     p_lastItemClicked = nullptr;
     m_drawAction = -1;
     m_allowItemScrollTmr = new QTimer(this);
-    connect(m_allowItemScrollTmr, SIGNAL(timeout()), this, SLOT(slotAllowItemScroll()));
+    connect(m_allowItemScrollTmr, &QTimer::timeout, this, &CMManager::slotAllowItemScroll);
 
     KConfigGroup grGen = KSharedConfig::openConfig()->group("General");
     slotSetManualRoute(grGen.readEntry("ManualRouting", false));
@@ -335,7 +335,8 @@ CanvasManipulator::CanvasManipulator(ItemDocument *itemDocument, CMManager *cmMa
     p_cnItemClickedOn = nullptr;
     p_cmManager = cmManager;
 
-    connect(itemDocument->canvas(), SIGNAL(resized(const QRect &, const QRect &)), this, SLOT(canvasResized(const QRect &, const QRect &)));
+    // connect(itemDocument->canvas(), SIGNAL(resized(const QRect &, const QRect &)), this, SLOT(canvasResized(const QRect &, const QRect &)));
+    connect(itemDocument->canvas(), &Canvas::resized, this, &CanvasManipulator::canvasResized);
 }
 
 CanvasManipulator::~CanvasManipulator()

commit 0253009ab021993748fb1e65f586ae774818e9f8

fix moving of components, so all text related to a component moves
before, the text of a component would not move together with the component
to which it belongs.
issue originally introduced in

5cd465d656712e2c5fefedd7e04b65ec7ca1701f
Author: Petros Siligkounas <petross404@gmail.com>  2021-04-26 14:20:51
Committer: Zoltan Padrah <zoltan.padrah@gmail.com>  2021-07-06 12:20:17
canvas: convert most connect/disconnect to new syntax

basic issue is a typo in the name of a signal

src/canvasitemparts.cpp
@@ -30,7 +30,7 @@ GuiPart::GuiPart(CNItem *parent, const QRect &r, KtlQCanvas *canvas)
     , b_pointsAdded(false)
     , m_originalRect(r)
 {
-    connect(parent, &CNItem::moveBy, this, &GuiPart::slotMoveBy);
+    connect(parent, &CNItem::movedBy, this, &GuiPart::slotMoveBy);
     setZ(parent->z() + 0.5);
 }
 